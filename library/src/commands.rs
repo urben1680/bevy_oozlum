d7865887f3b4f74b8a92a4d45ba9df67fdc1cc71
urben1680 <55257931+urben1680@users.noreply.github.com>
2022-09-15 09:42:29 +0200

    update

--- library/src/commands.rs
+++ library/src/commands.rs
@@ -2,66 +2,77 @@ use std::{marker::PhantomData, fmt::Debug, any::type_name, mem::{replace, size_o
 
 use bevy::{ecs::{system::Resource, query::WorldQuery}, prelude::{Commands, Entity, World, Component, Bundle, Without}, log::{error, warn, info}};
 
-use super::master_resource::{Master, MasterEntryTrait};
+use super::controller::Controller;
 
-pub struct Tiny<T>(pub T);
-
-impl<T> Tiny<T>{
-    fn new(inner: T) -> Self{
-        Self(inner)
-    }
+pub trait ReversibleCommand: Resource{
+    /// Initialize by mutating the world, has to include actions that occur in the `forward` method
+    fn init(&mut self, world: &mut World);
+    /// Deploy commands to redo the related actions
+    fn forward(&mut self, commands: &mut Commands);
+    /// Deploy commands to undo the related actions
+    fn backward(&mut self, commands: &mut Commands);
+    /// Cleanup commands like despawning buffers before Self is dropped
+    fn forget(&mut self, commands: &mut Commands);
 }
 
-pub trait BoxOrTiny: Send + Sync + 'static{
+/// Some LogCommands contain data that is moved on initializing.
+/// To keep the size of the trait object low past that, the value might be boxed.
+trait BoxOrTiny: Send + Sync + 'static + Sized{
     type Inner: Send + Sync;
-    fn to_inner(self) -> Self::Inner;
+    fn into_inner(self) -> Self::Inner;
 }
 
-impl<T: Send + Sync + 'static> BoxOrTiny for Box<T>{
-    type Inner = T;
-    fn to_inner(self) -> Self::Inner{
-        /*
-        const _: () = assert!(
-            size_of::<Box<T>>() < size_of::<Tiny<T>>, 
-            "{} should be put into the `Tiny` wrapper.", type_name::<T>()
-        );
-        */
-        *self
-    }
+trait BoxIsSmaller: Sized{
+    const BOX_IS_SMALLER: bool = size_of::<Self>() > size_of::<Box<Self>>();
 }
+
+impl<T> BoxIsSmaller for T{}
+
+struct Tiny<T>(T);
 
 impl<T: Send + Sync + 'static> BoxOrTiny for Tiny<T>{
     type Inner = T;
-    fn to_inner(self) -> Self::Inner{
-        /*
-        const _: () = assert!(
-            size_of::<Tiny<T>>() <= size_of::<Box<T>>, 
-            "{} should be put into a `Box`.", type_name::<T>()
-        );
-        */
+    fn into_inner(self) -> Self::Inner{
         self.0
     }
 }
 
-pub struct SpawnComponent<T: Component, Wrapped: BoxOrTiny<Inner = T>>{
+impl<T: Send + Sync + 'static> BoxOrTiny for Box<T>{
+    type Inner = T;
+    fn into_inner(self) -> Self::Inner{
+        *self
+    }
+}
+
+pub struct SpawnComponent;
+
+struct SpawnComponentVariant<T: Component, Wrapped: BoxOrTiny<Inner = T>>{
     data: Option<Wrapped>,
     entity: Entity,
     error: ErrorOption<SpawnComponentError>
 }
 
-impl<T: Component, Wrapped: BoxOrTiny<Inner = T>> SpawnComponent<T, Wrapped>{
-    pub fn new(entity: Entity, data: Wrapped, error: ErrorOption<SpawnComponentError>) -> Self{
-        Self{
-            data: Some(data),
-            entity,
-            error
+impl SpawnComponent{
+    pub fn new_box<T: Component>(entity: Entity, data: T, error: ErrorOption<SpawnComponentError>) -> Box<dyn ReversibleCommand>{
+        if T::BOX_IS_SMALLER{
+            Box::new(SpawnComponentVariant{
+                data: Some(Box::new(data)),
+                entity,
+                error
+            })
+        } else {
+            Box::new(SpawnComponentVariant{
+                data: Some(Tiny(data)),
+                entity,
+                error
+            })
         }
     }
 }
 
-impl<T: Component, Wrapped: BoxOrTiny<Inner = T>> MasterEntryTrait for SpawnComponent<T, Wrapped>{
+impl<T: Component, Wrapped: BoxOrTiny<Inner = T>> ReversibleCommand for SpawnComponentVariant<T, Wrapped>{
     fn init(&mut self, world: &mut World){
-        let value = replace(&mut self.data, None).unwrap().to_inner();
+        let value = replace(&mut self.data, None).unwrap().into_inner();
         if let Some(mut entity_mut) = world.get_entity_mut(self.entity){
             if !entity_mut.contains::<T>(){
                 entity_mut.insert(value);
@@ -102,16 +113,16 @@ pub struct DespawnComponent<T: Component>{
 }
 
 impl<T: Component> DespawnComponent<T>{
-    pub fn new(entity: Entity, error: ErrorOption<DespawnComponentError>) -> Self{
-        Self{
+    pub fn new_box(entity: Entity, error: ErrorOption<DespawnComponentError>) -> Box<dyn ReversibleCommand>{
+        Box::new(Self{
             entity,
             error,
             p: PhantomData
-        }
+        })
     }
 }
 
-impl<T: Component> MasterEntryTrait for DespawnComponent<T>{
+impl<T: Component> ReversibleCommand for DespawnComponent<T>{
     fn init(&mut self, world: &mut World){
         if let Some(mut entity_mut) = world.get_entity_mut(self.entity){
             if let Some(value) = entity_mut.remove::<T>(){
@@ -152,23 +163,32 @@ impl<T: Component> MasterEntryTrait for DespawnComponent<T>{
     }
 }
 
-pub struct SpawnResource<T: Resource, Wrapped: BoxOrTiny<Inner = T>>{
+pub struct SpawnResource;
+
+struct SpawnResourceVariant<T: Resource, Wrapped: BoxOrTiny<Inner = T>>{
     data: Option<Wrapped>,
     error: ErrorOption<SpawnResourceError>
 }
 
-impl<T: Resource, Wrapped: BoxOrTiny<Inner = T>> SpawnResource<T, Wrapped>{
-    pub fn new(data: Wrapped, error: ErrorOption<SpawnResourceError>) -> Self{
-        Self{
-            data: Some(data),
-            error
+impl SpawnResource{
+    pub fn new_box<T: Resource>(data: T, error: ErrorOption<SpawnResourceError>) -> Box<dyn ReversibleCommand>{
+        if T::BOX_IS_SMALLER{
+            Box::new(SpawnResourceVariant{
+                data: Some(Box::new(data)),
+                error
+            })
+        } else {
+            Box::new(SpawnResourceVariant{
+                data: Some(Tiny(data)),
+                error
+            })
         }
     }
 }
 
-impl<Inner: Resource, T: BoxOrTiny<Inner = Inner>> MasterEntryTrait for SpawnResource<Inner, T>{
+impl<Inner: Resource, T: BoxOrTiny<Inner = Inner>> ReversibleCommand for SpawnResourceVariant<Inner, T>{
     fn init(&mut self, world: &mut World){
-        let value = replace(&mut self.data, None).unwrap().to_inner();
+        let value = replace(&mut self.data, None).unwrap().into_inner();
         if !world.contains_resource::<Inner>(){
             world.insert_resource(value);
         } else {
@@ -200,15 +220,15 @@ pub struct DespawnResource<T: Resource>{
 }
 
 impl<T: Resource> DespawnResource<T>{
-    pub fn new(error: ErrorOption<DespawnResourceError>) -> Self{
-        Self{
+    pub fn new_box(error: ErrorOption<DespawnResourceError>) -> Box<dyn ReversibleCommand>{
+        Box::new(Self{
             error,
             p: PhantomData
-        }
+        })
     }
 }
 
-impl<T: Resource> MasterEntryTrait for DespawnResource<T>{
+impl<T: Resource> ReversibleCommand for DespawnResource<T>{
     fn init(&mut self, world: &mut World){
         if let Some(value) = world.remove_resource::<T>(){
             world.insert_resource(Despawned(value));
@@ -239,23 +259,32 @@ impl<T: Resource> MasterEntryTrait for DespawnResource<T>{
     }
 }
 
-pub struct SpawnEntity<T: Bundle, Wrapped: BoxOrTiny<Inner = T>>{
+pub struct SpawnEntity;
+
+struct SpawnEntityVariant<T: Bundle, Wrapped: BoxOrTiny<Inner = T>>{
     data: Option<Wrapped>,
     entity: Option<Entity>
 }
 
-impl<T: Bundle, Wrapped: BoxOrTiny<Inner = T>> SpawnEntity<T, Wrapped>{
-    pub fn new(data: Wrapped) -> Self{
-        Self{
-            data: Some(data),
-            entity: None
+impl SpawnEntity{
+    pub fn new_box<T: Bundle>(data: T) -> Box<dyn ReversibleCommand>{
+        if T::BOX_IS_SMALLER{
+            Box::new(SpawnEntityVariant{
+                data: Some(Box::new(data)),
+                entity: None
+            })
+        } else {
+            Box::new(SpawnEntityVariant{
+                data: Some(Tiny(data)),
+                entity: None
+            })
         }
     }
 }
 
-impl<T: Bundle, Wrapped: BoxOrTiny<Inner = T>> MasterEntryTrait for SpawnEntity<T, Wrapped>{
+impl<T: Bundle, Wrapped: BoxOrTiny<Inner = T>> ReversibleCommand for SpawnEntityVariant<T, Wrapped>{
     fn init(&mut self, world: &mut World){
-        let value = replace(&mut self.data, None).unwrap().to_inner();
+        let value = replace(&mut self.data, None).unwrap().into_inner();
         self.entity = Some(world.spawn().insert_bundle(value).id());
     }
     fn forward(&mut self, commands: &mut Commands){
@@ -281,15 +310,15 @@ pub struct DespawnEntity{
 }
 
 impl DespawnEntity{
-    pub fn new(entity: Entity, error: ErrorOption<DespawnEntityError>) -> Self{
-        Self{
+    pub fn new_box(entity: Entity, error: ErrorOption<DespawnEntityError>) -> Box<dyn ReversibleCommand>{
+        Box::new(Self{
             entity,
             error,
-        }
+        })
     }
 }
 
-impl MasterEntryTrait for DespawnEntity{
+impl ReversibleCommand for DespawnEntity{
     fn init(&mut self, world: &mut World){
         if let Some(mut entity_mut) = world.get_entity_mut(self.entity){
             entity_mut.insert(DespawnedEntity);
@@ -373,11 +402,11 @@ impl<E: Debug> ErrorOption<E>{
 pub struct LogCommands<'w, 's>(pub (super) Commands<'w, 's>);
 
 impl<'w, 's> LogCommands<'w, 's>{
-    pub fn add<T: MasterEntryTrait>(&mut self, mut command: T){
+    pub fn add(&mut self, mut command: Box<dyn ReversibleCommand>){
         self.0.add(move |world: &mut World|{
             command.init(world);
-            let mut master = world.resource_mut::<Master>();
-            master.log.back_mut().unwrap().push(Box::new(command));
+            let mut master = world.resource_mut::<Controller>();
+            master.log.back_mut().unwrap().push(command);
         })
     }
 }
