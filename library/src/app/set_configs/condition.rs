9b0c5c0a1b4d0281c28bb075e091940e19ee3991
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-09-19 16:51:43 +0200

    Update condition.rs

--- library/src/app/set_configs/condition.rs
+++ library/src/app/set_configs/condition.rs
@@ -5,33 +5,22 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use bevy::{
-    ecs::{
-        archetype::ArchetypeComponentId,
-        component::{ComponentId, Tick},
-        query::Access,
-        schedule::InternedSystemSet,
-        world::{unsafe_world_cell::UnsafeWorldCell, DeferredWorld},
-    },
-    prelude::{Condition, IntoSystem, ReadOnlySystem, Res, System, SystemSet, World},
-    utils::tracing::error
+use bevy::ecs::{
+    archetype::ArchetypeComponentId,
+    component::{ComponentId, Tick},
+    query::Access,
+    schedule::{Condition, InternedSystemSet},
+    system::{IntoSystem, ReadOnlySystem, System},
+    world::World,
+    world::{unsafe_world_cell::UnsafeWorldCell, DeferredWorld},
 };
 
 use crate::{
-    app::{check_tick, EMPTY_ARCHETYPE_COMPONENT_ACCESS, EMPTY_COMPONENT_ACCESS},
+    app::check_tick,
+    error_per_flag,
     log::{OutOfLog, RareTransitionLog},
     meta::{Direction, RevMeta},
 };
-
-macro_rules! error_once {
-    ($flag:expr, $($arg:tt)+) => ({
-        if !$flag {
-            error!($($arg)+);
-            $flag = true;
-        }
-        false
-    })
-}
 
 struct RevConditionForward<T> {
     condition: T,
@@ -42,20 +31,22 @@ struct RevConditionForward<T> {
     rev_meta_err: bool,
     lock_err: bool,
     out_of_log_err: bool,
-    direction_err: bool
+    direction_err: bool,
 }
 
 struct RevConditionBackward<In: Send + Sync + 'static> {
+    sets: Vec<InternedSystemSet>,
     log: Arc<Mutex<RareTransitionLog<()>>>,
     name: String,
-    tick: Tick,
     component_access: Access<ComponentId>,
     archetype_component_access: Access<ArchetypeComponentId>,
+    tick: Tick,
+    rev_meta_err: bool,
+    lock_err: bool,
+    out_of_log_err: bool,
+    direction_err: bool,
     _in: PhantomData<fn(In)>,
 }
-
-#[derive(SystemSet, Copy, Clone, Debug, Hash, PartialEq, Eq)]
-struct ConditionSet;
 
 pub(crate) fn forward_backward_conditions<
     In: Send + Sync + 'static,
@@ -65,6 +56,7 @@ pub(crate) fn forward_backward_conditions<
     condition: T,
 ) -> (impl Condition<(), In>, impl Condition<(), In>) {
     let condition: T::System = IntoSystem::into_system(condition);
+    let sets = condition.default_system_sets();
 
     let mut name_forward = condition.name().into_owned();
     let mut name_backward = name_forward.clone();
@@ -82,22 +74,27 @@ pub(crate) fn forward_backward_conditions<
         rev_meta_err: false,
         lock_err: false,
         out_of_log_err: false,
-        direction_err: false
+        direction_err: false,
     };
 
     let backward = RevConditionBackward {
+        sets,
         log,
         name: name_backward,
-        tick: Tick::new(0),
         component_access: Default::default(),
         archetype_component_access: Default::default(),
+        tick: Tick::new(0), // set by initialize
+        rev_meta_err: false,
+        lock_err: false,
+        out_of_log_err: false,
+        direction_err: false,
         _in: PhantomData,
     };
 
     (forward, backward)
 }
 
-impl<T: System<Out = bool> + ReadOnlySystem> System for RevConditionForward<T> {
+impl<T: ReadOnlySystem<Out = bool>> System for RevConditionForward<T> {
     type In = T::In;
     type Out = bool;
     fn name(&self) -> Cow<'static, str> {
@@ -123,39 +120,17 @@ impl<T: System<Out = bool> + ReadOnlySystem> System for RevConditionForward<T> {
     }
     unsafe fn run_unsafe(&mut self, input: Self::In, world: UnsafeWorldCell) -> Self::Out {
         let meta = unsafe {
-            // SAFETY: Registered read access to resource
+            // SAFETY:
+            // - Registered read access to resource
+            // - T cannot have write access because T: ReadOnlySystem
             world.get_resource::<RevMeta>()
         };
-        // no clone needed because it is expected that condition as a ReadOnlySystem would not mutate RevMeta
-        let Some(meta) = meta else {
-            return error_once!(self.rev_meta_err, "Reversible condition {} (forward) could not find RevMeta resource.", self.name);
-        };
-        let Ok(mut log) = self.log.try_lock() else {
-            return error_once!(self.lock_err, "Reversible condition {} (forward) could not lock internal log. \
-                This is likely an internal bug.", self.name);
-        };
-        match meta.get_direction() {
-            Some(Direction::Forward) => {
-                let out = unsafe {
-                    // SAFETY:
-                    // - condition registered it's own accesses that were used to update Self's accesses
-                    // - update_archetype_component_access was called by the caller of Self::run_unsafe
-                    self.condition.run_unsafe(input, world)
-                };
-                log.pop_past_by_len(meta.past_len());
-                log.push_present(out.then_some(().into()));
-                out
-            }
-            Some(Direction::ForwardLog) => match log.forward_log() {
-                Ok(option) => option.is_some(),
-                Err(OutOfLog) => error_once!(self.out_of_log_err, "Reversible condition {} (forward) got out of log. \
-                Make sure the reversible schedule this condition is in is correctly called in both the forward and backward direction. \
-                It seems that one or more backward schedule calls were missed. \
-                If this condition is in the RevUpdate schedule, this is likely an internal bug.", self.name)
-            }
-            _ => error_once!(self.direction_err, "Reversible condition {} (forward) did run while RevMeta was in a wrong direction ({:?}).\
-                If RevMeta was not manually overridden this is likely an internal bug.", self.name, meta.internal_direction()),
-        }
+        self.run_inner(meta, |cond| unsafe {
+            // SAFETY:
+            // - condition registered it's own accesses that were used to update Self's accesses
+            // - update_archetype_component_access was called by the caller of Self::run_unsafe
+            cond.run_unsafe(input, world)
+        })
     }
     fn run(&mut self, input: Self::In, world: &mut World) -> Self::Out {
         self.run_readonly(input, world)
@@ -167,19 +142,16 @@ impl<T: System<Out = bool> + ReadOnlySystem> System for RevConditionForward<T> {
         self.condition.queue_deferred(world)
     }
     fn initialize(&mut self, world: &mut World) {
-        let mut reads_meta = IntoSystem::into_system(|_: Res<RevMeta>| {});
-
         self.condition.initialize(world);
-        reads_meta.initialize(world);
-
-        self.component_access.extend(reads_meta.component_access());
         self.component_access
             .extend(self.condition.component_access());
-
-        self.archetype_component_access
-            .extend(reads_meta.archetype_component_access());
         self.archetype_component_access
             .extend(self.condition.archetype_component_access());
+        RevMeta::add_read_if_no_write(
+            world,
+            &mut self.component_access,
+            &mut self.archetype_component_access,
+        );
     }
     fn update_archetype_component_access(&mut self, world: UnsafeWorldCell) {
         self.condition.update_archetype_component_access(world);
@@ -190,7 +162,7 @@ impl<T: System<Out = bool> + ReadOnlySystem> System for RevConditionForward<T> {
         self.condition.check_change_tick(change_tick)
     }
     fn default_system_sets(&self) -> Vec<InternedSystemSet> {
-        vec![ConditionSet.intern()]
+        self.condition.default_system_sets()
     }
     fn get_last_run(&self) -> Tick {
         self.condition.get_last_run()
@@ -200,40 +172,11 @@ impl<T: System<Out = bool> + ReadOnlySystem> System for RevConditionForward<T> {
     }
 }
 
-// SAFETY: todo
-unsafe impl<T: System<Out = bool> + ReadOnlySystem> ReadOnlySystem for RevConditionForward<T> {
+// SAFETY: Self::run does ot mutate world
+unsafe impl<T: ReadOnlySystem<Out = bool>> ReadOnlySystem for RevConditionForward<T> {
     fn run_readonly(&mut self, input: Self::In, world: &World) -> Self::Out {
-        let Some(meta) = world.get_resource::<RevMeta>() else {
-            todo!()
-        };
-        let mut log = self.log.try_lock().unwrap();
-        match meta.get_direction() {
-            Some(Direction::Forward) => {
-                let out = self.condition.run_readonly(input, world);
-                log.pop_past_by_len(meta.past_len());
-                log.push_present(out.then_some(().into()));
-                out
-            }
-            Some(Direction::ForwardLog) => log.forward_log().expect("todo").is_some(),
-            _ => todo!(),
-        }
-    }
-}
-
-impl<In: Send + Sync + 'static> RevConditionBackward<In> {
-    fn run_inner(&mut self, meta: Option<&RevMeta>) -> bool {
-        if !matches!(
-            meta.and_then(RevMeta::get_direction),
-            Some(Direction::BackwardLog)
-        ) {
-            todo!()
-        }
-        self.log
-            .try_lock()
-            .unwrap()
-            .backward_log()
-            .expect("todo")
-            .is_some()
+        let meta = world.get_resource::<RevMeta>();
+        self.run_inner(meta, |cond| cond.run_readonly(input, world))
     }
 }
 
@@ -247,12 +190,10 @@ impl<In: Send + Sync + 'static> System for RevConditionBackward<In> {
         TypeId::of::<Self>()
     }
     fn component_access(&self) -> &Access<ComponentId> {
-        // todo add RevMeta read access
-        &EMPTY_COMPONENT_ACCESS
+        &self.component_access
     }
     fn archetype_component_access(&self) -> &Access<ArchetypeComponentId> {
-        // todo add RevMeta read access
-        &EMPTY_ARCHETYPE_COMPONENT_ACCESS
+        &self.archetype_component_access
     }
     fn is_send(&self) -> bool {
         true
@@ -271,24 +212,25 @@ impl<In: Send + Sync + 'static> System for RevConditionBackward<In> {
         self.run_inner(meta)
     }
     fn run(&mut self, _input: Self::In, world: &mut World) -> Self::Out {
-        self.run_inner(world.get_resource::<RevMeta>())
+        let meta = world.get_resource::<RevMeta>();
+        self.run_inner(meta)
     }
     fn apply_deferred(&mut self, _world: &mut World) {}
     fn queue_deferred(&mut self, _world: DeferredWorld) {}
     fn initialize(&mut self, world: &mut World) {
+        RevMeta::add_read_if_no_write(
+            world,
+            &mut self.component_access,
+            &mut self.archetype_component_access,
+        );
         self.tick = world.change_tick();
-        let mut reads_meta = IntoSystem::into_system(|_: Res<RevMeta>| {});
-        reads_meta.initialize(world);
-        self.component_access.extend(reads_meta.component_access());
-        self.archetype_component_access
-            .extend(reads_meta.archetype_component_access());
     }
     fn update_archetype_component_access(&mut self, _world: UnsafeWorldCell) {}
     fn check_change_tick(&mut self, change_tick: Tick) {
         check_tick(&mut self.tick, change_tick);
     }
     fn default_system_sets(&self) -> Vec<InternedSystemSet> {
-        vec![ConditionSet.intern()]
+        self.sets.clone()
     }
     fn get_last_run(&self) -> Tick {
         self.tick
@@ -300,6 +242,87 @@ impl<In: Send + Sync + 'static> System for RevConditionBackward<In> {
 
 unsafe impl<In: Send + Sync + 'static> ReadOnlySystem for RevConditionBackward<In> {
     fn run_readonly(&mut self, _input: Self::In, world: &World) -> Self::Out {
-        self.run_inner(world.get_resource::<RevMeta>())
+        let meta = world.get_resource::<RevMeta>();
+        self.run_inner(meta)
+    }
+}
+
+impl<T: ReadOnlySystem<Out = bool>> RevConditionForward<T> {
+    fn run_inner(
+        &mut self,
+        meta: Option<&RevMeta>,
+        eval_cond: impl FnOnce(&mut T) -> bool,
+    ) -> bool {
+        let Some(meta) = meta else {
+            return error_per_flag!(
+                &mut self.rev_meta_err,
+                "Reversible condition {} could not find RevMeta resource.",
+                self.name,
+            );
+        };
+        let mut log = match self.log.try_lock() {
+            Ok(log) => log,
+            Err(err) => {
+                return error_per_flag!(
+                    &mut self.lock_err,
+                    "Reversible condition {} could not acquire internal log ({err:?}). This is likely a crate bug.\n{meta:?}",
+                    self.name
+                )
+            }
+        };
+        match meta.get_direction() {
+            Some(Direction::Forward) => {
+                let out = eval_cond(&mut self.condition);
+                log.pop_past_by_len(meta.past_len().saturating_sub(1));
+                log.push_present(out.then_some(()));
+                out
+            }
+            Some(Direction::ForwardLog) => match log.forward_log() {
+                Ok(option) => option.is_some(),
+                Err(OutOfLog) => error_per_flag!(&mut self.out_of_log_err, "Reversible condition {} got out of log. \
+                    Make sure the reversible schedule this condition is in is correctly called in both the forward and backward direction. \
+                    It seems that one or more backward schedule calls were missed. \
+                    If this condition is in the RevUpdate schedule, this is likely a crate bug.\n{meta:?}", self.name)
+            }
+            _ => error_per_flag!(&mut self.direction_err, "Reversible condition {} did run while RevMeta was in a wrong direction. \
+                If RevMeta was not manually overwritten during a reversible schedule, this is likely a crate bug.\n{meta:?}", self.name),
+        }
+    }
+}
+
+impl<In: Send + Sync + 'static> RevConditionBackward<In> {
+    fn run_inner(&mut self, meta: Option<&RevMeta>) -> bool {
+        let Some(meta) = meta else {
+            return error_per_flag!(
+                &mut self.rev_meta_err,
+                "Reversible condition {} could not find RevMeta resource.",
+                self.name
+            );
+        };
+        if meta.get_direction() != Some(Direction::BackwardLog) {
+            return error_per_flag!(
+                &mut self.direction_err,
+                "Reversible condition {} did run while RevMeta was in a wrong direction. \
+                If RevMeta was not manually overwritten during a reversible schedule, this is likely a crate bug.\n{meta:?}",
+                self.name
+            );
+        }
+        let mut log = match self.log.try_lock() {
+            Ok(log) => log,
+            Err(err) => {
+                return error_per_flag!(
+                    &mut self.lock_err,
+                    "Reversible condition {} could not acquire internal log ({err:?}). This is likely a crate bug.\n{meta:?}",
+                    self.name
+                )
+            }
+        };
+        match log.backward_log() {
+            Ok(option) => option.is_some(),
+            Err(OutOfLog) => error_per_flag!(&mut self.out_of_log_err, "Reversible condition {} got out of log. \
+                Make sure the reversible schedule this condition is in is correctly called in both the forward and backward direction. \
+                It seems that one or more forward schedule calls were missed. \
+                If this condition is in the RevUpdate schedule, this is likely a crate bug.\n{meta:?}", self.name)
+        }
     }
 }
