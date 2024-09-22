4a05751a4fb5b8bce573dec0643a865d577e5a7a
urben1680 <urben1680@gmail.com>
2024-09-22 22:51:00 +0200

    began observer/event work

--- library/src/commands.rs
+++ library/src/commands.rs
@@ -1,13 +1,16 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, ops::Deref};
 
 use bevy::{
-    ecs::{component::ComponentId, storage::{ResourceData, SparseSet}, system::EntityCommands}, prelude::{Commands, Component, Entity, FromWorld, Resource, World}, utils::synccell::SyncCell
+    ecs::{observer::{TriggerEvent, TriggerTargets}, system::EntityCommands}, prelude::{Commands, Event, FromWorld, Resource, World}, utils::synccell::SyncCell
 };
 
 use crate::{
-    log::{OutOfLog, TransitionsLog, WithTimestamp},
+    log::{OutOfLog, StateLog, TransitionsLog, WithTimestamp},
     meta::{Direction, RevMeta},
 };
+
+mod bundle;
+mod observer;
 
 // todo: spawn/despawn with entity disabling https://github.com/bevyengine/bevy/issues/11090
 // todo: commands implementors https://docs.rs/bevy/latest/bevy/ecs/world/trait.Command.html#implementors
@@ -17,8 +20,9 @@ pub trait RevCommands {
     fn rev_add<Marker>(&mut self, command: impl RevCommand<Marker>);
     fn rev_init_resource<R: Resource + FromWorld>(&mut self);
     fn rev_insert_resource<R: Resource>(&mut self, resource: R);
-    fn remove_resource<R: Resource>(&mut self);
-    // todo: trigger?
+    fn rev_remove_resource<R: Resource>(&mut self);
+    fn rev_trigger(&mut self, event: impl Event);
+    fn rev_trigger_targets(&mut self, event: impl Event, targets: impl TriggerTargets);
 }
 
 impl RevCommands for Commands<'_, '_> {
@@ -48,10 +52,16 @@ impl RevCommands for Commands<'_, '_> {
             initiialized
         })
     }
-    fn remove_resource<R: Resource>(&mut self) {
+    fn rev_remove_resource<R: Resource>(&mut self) {
         self.rev_add(|world: &mut World| {
             world.remove_resource::<R>().map(|resource| ResourceSwap(Some(resource)))
         })
+    }
+    fn rev_trigger(&mut self, event: impl Event) {
+        
+    }
+    fn rev_trigger_targets(&mut self, event: impl Event, targets: impl TriggerTargets) {
+        
     }
 }
 
@@ -68,6 +78,47 @@ impl<R: Resource> InitializedRevCommand for ResourceSwap<R> {
     }
     fn redo(&mut self, world: &mut World) {
         self.undo(world)
+    }
+}
+
+/*
+Event does not impl Clone
+Could add observer for event so it is passed on to the log
+- pro: ~~no Clone bound on event needed~~ wrong, event is only available by reference
+
+- event can be mutated by the observer, needs to be reversible
+-- impl RevCommand only for RevEventMut<E>(StateLog<E>) and RevEvent<E>(E)
+--- downside: makes api uglier if bound is not Event but RevEvent: Event to only support immutable and logged event
+--- try to support more logs
+
+-- roadblock for mutable rev observer: ordering api is missing https://github.com/bevyengine/bevy/issues/14890
+--- do not offer logged events yet, warn about mutating events
+--- or offer an event wrapper that offers a data log where each observer can set data and only read data of previous observers
+---- data size: TriggerTargets.len()?
+- TriggerTargets could be reversed by rev command! but no order can be specified with rev_trigger (without targets)
+*/
+
+/// Wraps an event to only allow immutable access. 
+pub struct RevEvent<E>(E);
+
+impl<E> Deref for RevEvent<E> {
+    type Target = E;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct RevEventMut<E: Event>(pub StateLog<E>);
+
+impl<E: Event, Targets: TriggerTargets> RevCommand<()> for TriggerEvent<RevEvent<E>, Targets> {
+    fn rev_apply(self, world: &mut World) -> Option<impl InitializedRevCommand> {
+        todo!()
+    }
+}
+
+impl<E: Event, Targets: TriggerTargets> RevCommand<()> for TriggerEvent<RevEventMut<E>, Targets> {
+    fn rev_apply(self, world: &mut World) -> Option<impl InitializedRevCommand> {
+        todo!()
     }
 }
 
