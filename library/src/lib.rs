1af2566a55d10806164f2a3a05026178ed6f5ab5
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-09-25 17:38:29 +0200

    Update lib.rs

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -2,7 +2,6 @@
 TODO:
 
 Features:
-- hooks
 - reversible Event reader/writer
 - entity commands, standard rev commands
 -- postponed to bevy 0.15 due to required components + disabled entities
@@ -16,9 +15,20 @@ Enhancements:
 - config tests
 - make doctests work
 - mod/use/pub cleanup, make prelude
-- setting for state logs that serde only takes the current state into account
+- setting for state logs that serde only takes the current state into account (default?)
+- setting for logs to serde the current capacity as well
+-- all as functions for #[serde(with = "module")], uses $module::serialize and $module::deserialize
+                            State   Transition
+default                     full    full
+state_only                  x       -
+with_capacity               x       x
+state_only_with_capacity    x       -
+
 - observer tests
-- commands -> observer -> hook reversible order test
+- hook -> observer -> commands reversible order test
+-- commands should also trigger hooks here
+-- commands shouls also call observer here
+-- a second sync point aftwerwards should be scheduled to assert nothing ist postponed into it
 
 Docs
 - examples
@@ -41,6 +51,7 @@ use bevy::{
     ecs::schedule::{InternedScheduleLabel, InternedSystemSet, IntoSystemConfigs, ScheduleLabel},
 };
 
+use commands::RevCommandBuffer;
 use meta::RevMeta;
 
 pub mod app;
@@ -57,21 +68,9 @@ pub mod world;
 #[derive(ScheduleLabel, Copy, Clone, Debug, Hash, PartialEq, Eq)]
 struct ForwardSchedule(InternedScheduleLabel);
 
-impl ForwardSchedule {
-    fn of(label: impl ScheduleLabel) -> Self {
-        Self(label.intern())
-    }
-}
-
 /// Should not be pub to not add invalid settings (see unsupported Schedule settings)
 #[derive(ScheduleLabel, Copy, Clone, Debug, Hash, PartialEq, Eq)]
 struct BackwardSchedule(InternedScheduleLabel);
-
-impl BackwardSchedule {
-    fn of(label: impl ScheduleLabel) -> Self {
-        Self(label.intern())
-    }
-}
 
 #[derive(ScheduleLabel, Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub struct RevUpdate;
@@ -92,7 +91,9 @@ impl Default for RevSystemsPlugin {
 
 impl Plugin for RevSystemsPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.register_type::<RevMeta>();
+        app.register_type::<RevMeta>()
+            // needs to be manually inserted because first accessor might be a hook which cannot insert it
+            .init_resource::<RevCommandBuffer>();
 
         if let Some(rev_meta) = &self.rev_meta {
             app.insert_resource(rev_meta.clone());
