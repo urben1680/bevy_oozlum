0facb6d4a0b350ddede808a7813aa8b645cccee1
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-10-09 16:56:13 +0200

    Update rare_transition.rs

--- library/src/log/rare_transition.rs
+++ library/src/log/rare_transition.rs
@@ -291,7 +291,7 @@ impl<T: LoggedAt> RareTransitionLog<T> {
             .drain(..partition_point)
             .map(|rare| rare.value)
     }
-    pub fn reduce_timestamps(&mut self, by: usize) -> impl LogIter<T> {
+    pub fn reduce_logged_at(&mut self, by: usize) -> impl LogIter<T> {
         let reduced_at = self
             .transitions
             .range_mut(..self.index)
@@ -368,7 +368,7 @@ mod test {
 
             let with_timestamp = transition.map(|transition| self.meta.with_logged_at(transition));
 
-            self.with_timestamp[0].pop_past_by_timestamp(self.meta.log_range().start);
+            self.with_timestamp[0].pop_past_by_timestamp(*self.meta.log_range().start());
             let middle = self.with_timestamp[0].clone();
             self.with_timestamp[0].push_present(with_timestamp);
             assert!(
@@ -387,7 +387,7 @@ mod test {
                 self.with_timestamp[0]
             );
 
-            let _ = self.with_timestamp[1].drain_past_by_timestamp(self.meta.log_range().start);
+            let _ = self.with_timestamp[1].drain_past_by_timestamp(*self.meta.log_range().start());
             let middle = self.with_timestamp[1].clone();
             self.with_timestamp[1].push_present(with_timestamp);
             assert!(
