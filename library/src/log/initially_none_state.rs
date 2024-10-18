6d446c5093c6b5abd3b67ce3315b1b38db9c0b12
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-10-18 17:10:56 +0200

    Update initially_none_state.rs

--- library/src/log/initially_none_state.rs
+++ library/src/log/initially_none_state.rs
@@ -33,8 +33,8 @@ mod serde_with {
         fn get_logless_state(&self) -> Self::Se<'_> {
             self.get()
         }
-        fn from_logless_state(logless_state: Self::De) -> Self {
-            logless_state.into()
+        fn from_logless_state(logless_state: Self::De) -> Result<Self, String> {
+            Ok(logless_state.into())
         }
     }
 
@@ -56,17 +56,20 @@ mod serde_with {
                 },
             }
         }
-        fn from_with_capacity(with_capacity: Self::De) -> Self {
-            Self(match with_capacity {
+        fn from_with_capacity(with_capacity: Self::De) -> Result<Self, String> {
+            Ok(Self(match with_capacity {
                 Inner::NeverRan { capacity } => Inner::NeverRan { capacity },
                 Inner::Ran {
                     log,
                     undone_first_run,
-                } => Inner::Ran {
-                    log: StateLog::from_with_capacity(log),
-                    undone_first_run,
-                },
-            })
+                } => match StateLog::from_with_capacity(log) {
+                    Ok(log) => Inner::Ran {
+                        log,
+                        undone_first_run,
+                    },
+                    Err(err) => return Err(err)
+                }
+            }))
         }
     }
 
@@ -85,8 +88,8 @@ mod serde_with {
                 } => ((!undone_first_run).then_some(&*log), log.capacity()),
             }
         }
-        fn from_logless_with_capacity(logless_with_capacity: Self::De) -> Self {
-            Self(match logless_with_capacity.0 {
+        fn from_logless_with_capacity(logless_with_capacity: Self::De) -> Result<Self, String> {
+            Ok(Self(match logless_with_capacity.0 {
                 Some(present) => Inner::Ran {
                     log: StateLog::with_capacity(present, logless_with_capacity.1),
                     undone_first_run: false,
@@ -94,7 +97,7 @@ mod serde_with {
                 None => Inner::NeverRan {
                     capacity: logless_with_capacity.1,
                 },
-            })
+            }))
         }
     }
 }
@@ -137,17 +140,6 @@ impl<T> InitiallyNoneStateLog<T> {
         } = &self.0
         {
             Some(&*log)
-        } else {
-            None
-        }
-    }
-    pub fn unlogged_get_mut(&mut self) -> Option<&mut T> {
-        if let Inner::Ran {
-            log,
-            undone_first_run: false,
-        } = &mut self.0
-        {
-            Some(log.unlogged_get_mut())
         } else {
             None
         }
@@ -214,6 +206,7 @@ impl<T> InitiallyNoneStateLog<T> {
             }
         }
     }
+    // todo: may incorrectly return `Ok` if `clear` was called before
     pub fn forward_log(&mut self) -> Result<&mut StateLog<T>, OutOfLog> {
         match &mut self.0 {
             Inner::Ran {
@@ -230,6 +223,7 @@ impl<T> InitiallyNoneStateLog<T> {
             Inner::NeverRan { .. } => Err(OutOfLog),
         }
     }
+    // todo: may incorrectly return `Ok` if constructed from T
     pub fn backward_log(&mut self) -> Result<&mut StateLog<T>, OutOfLog> {
         match &mut self.0 {
             Inner::Ran {
