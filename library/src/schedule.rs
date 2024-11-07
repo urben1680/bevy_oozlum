f8c510b4402d96c21dacd3ad9083bf77abe22812
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-11-07 17:12:25 +0100

    Update schedule.rs

--- library/src/schedule.rs
+++ library/src/schedule.rs
@@ -191,18 +191,18 @@ mod test {
     enum Test<T> {
         Sys(T),
 
-        HookBySys(T),
-        HookByCmd(T),
+        SysHook(T),
+        SysHookObsv(T),
+        SysHookCmd(T),
 
-        ObsvBySys(T),
-        ObsvByCmd(T),
-        ObsvByHook(T),
-        ObsvByObsv(T),
+        SysObsv(T),
+        SysObsvObsv(T),
+        SysObsvCmd(T),
 
-        CmdBySys(T),
-        CmdByCmd(T),
-        CmdByHook(T),
-        CmdByObsv(T),
+        SysCmd(T),
+        SysCmdHook(T),
+        SysCmdObsv(T),
+        SysCmdCmd(T),
     }
 
     impl<T> Test<T> {
@@ -210,42 +210,84 @@ mod test {
             match self {
                 Test::Sys(value) => Test::Sys(map(value)),
 
-                Test::HookBySys(value) => Test::HookBySys(map(value)),
-                Test::HookByCmd(value) => Test::HookByCmd(map(value)),
+                Test::SysHook(value) => Test::SysHook(map(value)),
+                Test::SysHookObsv(value) => Test::SysHookObsv(map(value)),
+                Test::SysHookCmd(value) => Test::SysHookCmd(map(value)),
 
-                Test::ObsvBySys(value) => Test::ObsvBySys(map(value)),
-                Test::ObsvByCmd(value) => Test::ObsvByCmd(map(value)),
-                Test::ObsvByHook(value) => Test::ObsvByHook(map(value)),
-                Test::ObsvByObsv(value) => Test::ObsvByObsv(map(value)),
+                Test::SysObsv(value) => Test::SysObsv(map(value)),
+                Test::SysObsvObsv(value) => Test::SysObsvObsv(map(value)),
+                Test::SysObsvCmd(value) => Test::SysObsvCmd(map(value)),
 
-                Test::CmdBySys(value) => Test::CmdBySys(map(value)),
-                Test::CmdByCmd(value) => Test::CmdByCmd(map(value)),
-                Test::CmdByHook(value) => Test::CmdByHook(map(value)),
-                Test::CmdByObsv(value) => Test::CmdByObsv(map(value)),
+                Test::SysCmd(value) => Test::SysCmd(map(value)),
+                Test::SysCmdHook(value) => Test::SysCmdHook(map(value)),
+                Test::SysCmdObsv(value) => Test::SysCmdObsv(map(value)),
+                Test::SysCmdCmd(value) => Test::SysCmdCmd(map(value)),
             }
         }
     }
 
-    #[derive(Resource)]
+    #[derive(Clone, Copy)]
+    enum TestBundle {
+        Regular(u8),
+        RegularSyncPoint(u8),
+        Exclusive(u8),
+    }
+
+    impl IntoIterator for TestBundle {
+        type IntoIter = std::vec::IntoIter<Test<u8>>;
+        type Item = Test<u8>;
+        fn into_iter(self) -> Self::IntoIter {
+            match self {
+                Self::Regular(n) => vec![
+                    Test::Sys(n),
+                    Test::SysObsv(n),
+                    Test::SysObsvObsv(n),
+                    Test::SysObsvCmd(n),
+                ],
+                Self::RegularSyncPoint(n) => vec![
+                    Test::SysCmd(n),
+                    Test::SysCmdHook(n),
+                    Test::SysCmdObsv(n),
+                    Test::SysCmdCmd(n),
+                ],
+                Self::Exclusive(n) => vec![
+                    Test::Sys(n),
+                    Test::SysHook(n),
+                    Test::SysHookObsv(n),
+                    Test::SysHookCmd(n),
+                    Test::SysObsv(n),
+                    Test::SysObsvObsv(n),
+                    Test::SysObsvCmd(n),
+                    Test::SysCmd(n),
+                    Test::SysCmdHook(n),
+                    Test::SysCmdObsv(n),
+                    Test::SysCmdCmd(n),
+                ],
+            }
+            .into_iter()
+        }
+    }
+
+    #[derive(Resource, Default)]
     struct TestLog(Vec<Test<(u8, RevDirection)>>);
 
     #[derive(Component)]
-    struct HookBySys(u8);
+    struct SysHook(u8);
 
     #[derive(Component)]
-    struct HookByCmd(u8);
+    struct SysCmdHook(u8);
 
     #[derive(Event, Clone)]
-    struct ObsvBySys(u8);
+    struct SysObsv(u8);
 
     #[derive(Event, Clone)]
-    struct ObsvByCmd(u8);
+    struct SysHookObsv(u8);
 
     #[derive(Event, Clone)]
-    struct ObsvByHook(u8);
+    struct SysObsvObsv(u8);
 
     #[derive(Event, Clone)]
-    struct ObsvByObsv(u8);
+    struct SysCmdObsv(u8);
 
     /// Has deferred params, will add sync point
     fn regular_system<const N: u8>(mut world: DeferredWorld, mut commands: Commands) {
@@ -259,7 +301,7 @@ mod test {
         }
 
         // trigger observer in system
-        world.rev_trigger(ObsvBySys(N));
+        world.rev_trigger(SysObsv(N));
 
         // trigger command in system
         commands.rev_queue(system_command::<N>);
@@ -277,10 +319,10 @@ mod test {
         }
 
         // trigger hook in system
-        world.spawn(HookBySys(N));
+        world.spawn(SysHook(N));
 
         // trigger observer in system
-        world.rev_trigger(ObsvBySys(N));
+        world.rev_trigger(SysObsv(N));
 
         // trigger command in system
         world.commands().rev_queue(system_command::<N>);
@@ -290,20 +332,20 @@ mod test {
         world
             .resource_mut::<TestLog>()
             .0
-            .push(Test::CmdBySys((N, RevDirection::NotLog)));
+            .push(Test::SysCmd((N, RevDirection::NotLog)));
 
         // trigger hook in command
-        world.spawn(HookByCmd(N));
+        world.spawn(SysCmdHook(N));
 
         // trigger observer in command
-        world.rev_trigger(ObsvByCmd(N));
+        world.rev_trigger(SysCmdObsv(N));
 
         // trigger command in command
         world.commands().rev_queue(|world: &mut World| {
             world
                 .resource_mut::<TestLog>()
                 .0
-                .push(Test::CmdByCmd((N, RevDirection::NotLog)));
+                .push(Test::SysCmdCmd((N, RevDirection::NotLog)));
 
             |world: &mut World, forward: bool| {
                 let direction = match forward {
@@ -313,7 +355,7 @@ mod test {
                 world
                     .resource_mut::<TestLog>()
                     .0
-                    .push(Test::CmdByCmd((N, direction)));
+                    .push(Test::SysCmdCmd((N, direction)));
             }
         });
 
@@ -325,13 +367,17 @@ mod test {
             world
                 .resource_mut::<TestLog>()
                 .0
-                .push(Test::CmdBySys((N, direction)));
+                .push(Test::SysCmd((N, direction)));
         }
     }
 
-    fn test_run(configs: impl FnOnce(&mut Schedule), tests: Vec<Vec<Test<u8>>>) {
+    fn test_run(
+        configs: impl FnOnce(&mut Schedule) -> &mut Schedule,
+        expected: Vec<Vec<TestBundle>>,
+    ) {
         // set up world
         let mut world = World::new();
+        world.init_resource::<TestLog>();
         world.insert_resource(RevMeta::new(None, 0, false));
         let mut schedule = Schedule::new(FixedUpdate);
         schedule.add_systems(RevMeta::update_world);
@@ -345,24 +391,24 @@ mod test {
         world.add_schedule(schedule);
 
         // set up observers
-        world.observe(
-            |trigger: Trigger<RevEvent<ObsvBySys>>, mut world: DeferredWorld| {
+        world.rev_observe(
+            |trigger: Trigger<RevEvent<SysObsv>>, mut world: DeferredWorld| {
                 let event = trigger.event();
                 let n = event.0;
                 world
                     .resource_mut::<TestLog>()
                     .0
-                    .push(Test::ObsvBySys((n, event.direction())));
+                    .push(Test::SysObsv((n, event.direction())));
 
                 // trigger observer in observer
-                world.rev_trigger(ObsvByHook(n));
+                world.rev_trigger(SysObsvObsv(n));
 
                 // trigger command in observer
                 world.commands().rev_queue(move |world: &mut World| {
                     world
                         .resource_mut::<TestLog>()
                         .0
-                        .push(Test::CmdByObsv((n, RevDirection::NotLog)));
+                        .push(Test::SysObsvCmd((n, RevDirection::NotLog)));
 
                     move |world: &mut World, forward: bool| {
                         let direction = match forward {
@@ -372,55 +418,55 @@ mod test {
                         world
                             .resource_mut::<TestLog>()
                             .0
-                            .push(Test::CmdByObsv((n, direction)));
+                            .push(Test::SysObsvCmd((n, direction)));
                     }
                 });
             },
         );
-        world.observe(
-            |trigger: Trigger<RevEvent<ObsvByCmd>>, mut log: ResMut<TestLog>| {
+        world.rev_observe(
+            |trigger: Trigger<RevEvent<SysHookObsv>>, mut log: ResMut<TestLog>| {
                 let event = trigger.event();
-                log.0.push(Test::ObsvByCmd((event.0, event.direction())));
+                log.0.push(Test::SysHookObsv((event.0, event.direction())));
             },
         );
-        world.observe(
-            |trigger: Trigger<RevEvent<ObsvByHook>>, mut log: ResMut<TestLog>| {
+        world.rev_observe(
+            |trigger: Trigger<RevEvent<SysObsvObsv>>, mut log: ResMut<TestLog>| {
                 let event = trigger.event();
-                log.0.push(Test::ObsvByHook((event.0, event.direction())));
+                log.0.push(Test::SysObsvObsv((event.0, event.direction())));
             },
         );
-        world.observe(
-            |trigger: Trigger<RevEvent<ObsvByObsv>>, mut log: ResMut<TestLog>| {
+        world.rev_observe(
+            |trigger: Trigger<RevEvent<SysCmdObsv>>, mut log: ResMut<TestLog>| {
                 let event = trigger.event();
-                log.0.push(Test::ObsvByObsv((event.0, event.direction())));
+                log.0.push(Test::SysCmdObsv((event.0, event.direction())));
             },
         );
 
         // set up hooks
-        world.rev_register_component_hooks::<HookBySys>().on_add(
+        world.rev_register_component_hooks::<SysHook>().on_add(
             |direction, mut world, entity, _| {
                 let Ok(direction): Result<RevDirection, _> = direction.try_into() else {
                     return;
                 };
-                let n = world.entity(entity).get::<HookBySys>().expect("todo").0;
+                let n = world.entity(entity).get::<SysHook>().expect("todo").0;
                 world
                     .resource_mut::<TestLog>()
                     .0
-                    .push(Test::HookBySys((n, direction)));
+                    .push(Test::SysHook((n, direction)));
 
                 if direction != RevDirection::NotLog {
                     return;
                 }
 
                 // trigger observer in hook
-                world.rev_trigger(ObsvByHook(n));
+                world.rev_trigger(SysHookObsv(n));
 
                 // trigger command in hook
                 world.commands().rev_queue(move |world: &mut World| {
                     world
                         .resource_mut::<TestLog>()
                         .0
-                        .push(Test::CmdByHook((n, RevDirection::NotLog)));
+                        .push(Test::SysHookCmd((n, RevDirection::NotLog)));
 
                     move |world: &mut World, forward: bool| {
                         let direction = match forward {
@@ -430,64 +476,81 @@ mod test {
                         world
                             .resource_mut::<TestLog>()
                             .0
-                            .push(Test::CmdByHook((n, direction)));
+                            .push(Test::SysHookCmd((n, direction)));
                     }
                 });
             },
         );
-        world.rev_register_component_hooks::<HookByCmd>().on_add(
+        world.rev_register_component_hooks::<SysCmdHook>().on_add(
             |direction, mut world, entity, _| {
                 let Ok(direction) = direction.try_into() else {
                     return;
                 };
-                let n = world.entity(entity).get::<HookByCmd>().expect("todo").0;
+                let n = world.entity(entity).get::<SysCmdHook>().expect("todo").0;
                 world
                     .resource_mut::<TestLog>()
                     .0
-                    .push(Test::HookByCmd((n, direction)));
+                    .push(Test::SysCmdHook((n, direction)));
             },
         );
 
-        // run tests forward
-        for (i, step) in tests.iter().enumerate() {
-            let tests = step
-                .iter()
-                .map(|test| test.map(|n| (n, RevDirection::NotLog)))
-                .collect::<Vec<_>>();
+        fn test_step(
+            world: &mut World,
+            step: usize,
+            expected: &Vec<TestBundle>,
+            direction: RevDirection,
+        ) {
             world.run_schedule(FixedUpdate);
-            let log = &mut world.resource_mut::<TestLog>().0;
-            let log = take(log);
-            assert_eq!(tests, log, "\nforward step: {i}");
+            let actual = take(&mut world.resource_mut::<TestLog>().0);
+            let iter = expected
+                .iter()
+                .flat_map(|bundle| bundle.into_iter())
+                .map(|test| test.map(|n| (n, direction)));
+            let expected: Vec<_> = if direction.is_forward() {
+                iter.collect()
+            } else {
+                iter.rev().collect()
+            };
+            assert_eq!(actual, expected, "{direction:?} step #{step}");
+        }
+
+        // run tests forward
+        for (step, expected) in expected.iter().enumerate() {
+            test_step(&mut world, step, expected, RevDirection::NotLog);
         }
 
         // run tests backward log
         let mut meta = world.resource_mut::<RevMeta>();
         let end_frame = meta.present_world_state();
         assert!(meta.queue_log(RevFrame::new(0)).is_ok());
-        for (i, step) in tests.iter().enumerate().rev() {
-            let tests = step
-                .iter()
-                .rev()
-                .map(|test| test.map(|n| (n, RevDirection::BackwardLog)))
-                .collect::<Vec<_>>();
-            world.run_schedule(FixedUpdate);
-            let log = &mut world.resource_mut::<TestLog>().0;
-            let log = take(log);
-            assert_eq!(tests, log, "\nbackward log step: {i}");
+        for (step, expected) in expected.iter().enumerate().rev() {
+            test_step(&mut world, step, expected, RevDirection::BackwardLog);
         }
 
         // run tests forward log
         let mut meta = world.resource_mut::<RevMeta>();
         assert!(meta.queue_log(end_frame).is_ok());
-        for (i, step) in tests.iter().enumerate() {
-            let tests = step
-                .iter()
-                .map(|test| test.map(|n| (n, RevDirection::ForwardLog)))
-                .collect::<Vec<_>>();
-            world.run_schedule(FixedUpdate);
-            let log = &mut world.resource_mut::<TestLog>().0;
-            let log = take(log);
-            assert_eq!(tests, log, "\nforward log step: {i}");
+        for (step, expected) in expected.iter().enumerate() {
+            test_step(&mut world, step, expected, RevDirection::ForwardLog);
         }
+    }
+
+    #[test]
+    fn single_regular_system() {
+        test_run(
+            |schedule| schedule.add_systems(regular_system::<1>),
+            vec![vec![
+                TestBundle::Regular(1),
+                TestBundle::RegularSyncPoint(1),
+            ]],
+        );
+    }
+
+    #[test]
+    fn single_exclusive_system() {
+        test_run(
+            |schedule| schedule.add_systems(exclusive_system::<1>),
+            vec![vec![TestBundle::Exclusive(1)]],
+        );
     }
 }
