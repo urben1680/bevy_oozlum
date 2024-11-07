245fa23493a110468d712c4637abb47fd3dd151b
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-11-07 17:12:39 +0100

    Update world.rs

--- library/src/world.rs
+++ library/src/world.rs
@@ -1,13 +1,28 @@
-use bevy::ecs::{
-    component::{Component, ComponentId},
-    event::Event,
-    observer::{TriggerEvent, TriggerTargets},
-    world::{DeferredWorld, World},
+use bevy::{
+    ecs::{
+        component::{Component, ComponentId},
+        event::Event,
+        observer::{TriggerEvent, TriggerTargets},
+        system::IntoObserverSystem,
+        world::{DeferredWorld, World},
+    },
+    prelude::{Bundle, EntityWorldMut},
 };
 
-use crate::{commands::buffer_rev_command, hook::RevComponentHooks, observer::apply_trigger_event};
+use crate::{
+    commands::buffer_rev_command,
+    hook::RevComponentHooks,
+    observer::{apply_trigger_event, ObserverLog, RevEvent},
+};
 
 pub trait RevWorld {
+    fn rev_observe<E, B, M>(
+        &mut self,
+        system: impl IntoObserverSystem<RevEvent<E>, B, M>,
+    ) -> EntityWorldMut<'_>
+    where
+        E: Event + Clone,
+        B: Bundle;
     fn rev_trigger(&mut self, event: impl Event + Clone);
     fn rev_trigger_targets(&mut self, event: impl Event + Clone, targets: impl TriggerTargets);
     fn rev_register_component_hooks<T: Component>(&mut self) -> RevComponentHooks;
@@ -20,6 +35,17 @@ pub trait RevDeferredWorld {
 }
 
 impl RevWorld for World {
+    fn rev_observe<E, B, M>(
+        &mut self,
+        system: impl IntoObserverSystem<RevEvent<E>, B, M>,
+    ) -> EntityWorldMut<'_>
+    where
+        E: Event + Clone,
+        B: Bundle,
+    {
+        self.init_resource::<ObserverLog<E>>();
+        self.observe(system)
+    }
     fn rev_trigger(&mut self, event: impl Event + Clone) {
         let mut world: DeferredWorld = self.into();
         world.rev_trigger(event);
