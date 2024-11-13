b0dc06f7406d3e8aa41ee704e600622a3bd152f3
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-11-13 16:54:24 +0100

    Update init_none.rs

--- library/src/log/init_none.rs
+++ library/src/log/init_none.rs
@@ -1,16 +1,29 @@
+use std::{
+    collections::{TryReserveError, VecDeque},
+    mem::take,
+};
+
 use bevy::reflect::Reflect;
 
-use super::{OutOfLog, StateLog};
+use crate::meta::RevMeta;
+
+use super::{LogIter, LoggedAt, OutOfLog, StateLog};
 
 #[derive(Debug, Clone, Reflect)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct InitNoneLog<T>(Inner<StateLog<T>>);
+pub struct InitNoneLog<T>(Inner<T>);
 
 #[derive(Debug, Clone, Reflect)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub(super) enum Inner<T> {
-    NeverRan { capacity: usize },
-    Ran { log: T, undone_first_run: bool },
+enum Inner<T> {
+    NeverRan {
+        empty: VecDeque<T>,
+    },
+    Ran {
+        log: StateLog<T>,
+        /// If `None`, own none state is out of log or was never init as none.
+        undone_first_run: Option<bool>,
+    },
 }
 
 #[cfg(feature = "serde")]
@@ -19,7 +32,13 @@ mod serde_with {
 
     use crate::log::serde_with::{LoglessState, LoglessWithCapacity, WithCapacity};
 
-    use super::{InitNoneLog, Inner, StateLog};
+    use super::*;
+
+    #[derive(serde::Serialize, serde::Deserialize)]
+    pub(crate) enum InnerWithCapacity<T> {
+        NeverRan { capacity: usize },
+        Ran { log: T, undone_first_run: Option<bool> },
+    }
 
     impl<T> LoglessState for InitNoneLog<T>
     where
@@ -39,15 +58,17 @@ mod serde_with {
     where
         T: Serialize + for<'de> Deserialize<'de> + 'static,
     {
-        type Se<'se> = Inner<<StateLog<T> as WithCapacity>::Se<'se>>;
-        type De = Inner<<StateLog<T> as WithCapacity>::De>;
+        type Se<'se> = InnerWithCapacity<<StateLog<T> as WithCapacity>::Se<'se>>;
+        type De = InnerWithCapacity<<StateLog<T> as WithCapacity>::De>;
         fn get_with_capacity(&self) -> Self::Se<'_> {
             match self.0 {
-                Inner::NeverRan { capacity } => Inner::NeverRan { capacity },
+                Inner::NeverRan { ref empty } => InnerWithCapacity::NeverRan {
+                    capacity: empty.capacity(),
+                },
                 Inner::Ran {
                     ref log,
                     undone_first_run,
-                } => Inner::Ran {
+                } => InnerWithCapacity::Ran {
                     log: log.get_with_capacity(),
                     undone_first_run,
                 },
@@ -55,8 +76,10 @@ mod serde_with {
         }
         fn from_with_capacity(with_capacity: Self::De) -> Self {
             Self(match with_capacity {
-                Inner::NeverRan { capacity } => Inner::NeverRan { capacity },
-                Inner::Ran {
+                InnerWithCapacity::NeverRan { capacity } => Inner::NeverRan {
+                    empty: VecDeque::with_capacity(capacity),
+                },
+                InnerWithCapacity::Ran {
                     log,
                     undone_first_run,
                 } => Inner::Ran {
@@ -75,20 +98,20 @@ mod serde_with {
         type De = (Option<T>, usize);
         fn get_logless_with_capacity(&self) -> Self::Se<'_> {
             match self.0 {
-                Inner::NeverRan { capacity } => (None, capacity),
-                Inner::Ran {
-                    ref log,
-                    undone_first_run,
-                } => ((!undone_first_run).then_some(&*log), log.states_capacity()),
+                Inner::NeverRan { ref empty } => (None, empty.capacity()),
+                Inner::Ran { ref log, undone_first_run: Some(true) } => (None, log.states_capacity()),
+                Inner::Ran { ref log, .. } => (Some(&*log), log.states_capacity()),
             }
         }
         fn from_logless_with_capacity((state, capacity): Self::De) -> Self {
             Self(match state {
                 Some(present) => Inner::Ran {
                     log: StateLog::with_capacity(present, capacity),
-                    undone_first_run: false,
+                    undone_first_run: None,
                 },
-                None => Inner::NeverRan { capacity },
+                None => Inner::NeverRan {
+                    empty: VecDeque::with_capacity(capacity),
+                },
             })
         }
     }
@@ -96,7 +119,7 @@ mod serde_with {
 
 impl<T> Default for InitNoneLog<T> {
     fn default() -> Self {
-        Self::new()
+        Self::new_none()
     }
 }
 
@@ -104,7 +127,7 @@ impl<T> From<T> for InitNoneLog<T> {
     fn from(present: T) -> Self {
         Self(Inner::Ran {
             log: present.into(),
-            undone_first_run: false,
+            undone_first_run: None,
         })
     }
 }
@@ -113,149 +136,284 @@ impl<T> From<Option<T>> for InitNoneLog<T> {
     fn from(value: Option<T>) -> Self {
         match value {
             Some(present) => present.into(),
-            None => Self::new(),
+            None => Self::new_none(),
         }
     }
 }
 
-impl<T> InitNoneLog<T> {
-    pub const fn new() -> Self {
-        Self::with_capacity(0)
+impl<T: PartialEq> PartialEq<Option<T>> for InitNoneLog<T> {
+    fn eq(&self, other: &Option<T>) -> bool {
+        self.get() == other.as_ref()
     }
-    pub const fn with_capacity(capacity: usize) -> Self {
-        Self(Inner::NeverRan { capacity })
+}
+
+impl<T: PartialEq> PartialEq<Option<&T>> for InitNoneLog<T> {
+    fn eq(&self, other: &Option<&T>) -> bool {
+        self.get() == *other
+    }
+}
+
+impl<T> InitNoneLog<T> {
+    pub const fn new_none() -> Self {
+        Self(Inner::NeverRan {
+            empty: VecDeque::new(),
+        })
+    }
+    pub const fn new_some(present: T) -> Self {
+        Self(Inner::Ran {
+            log: StateLog::new(present),
+            undone_first_run: None,
+        })
+    }
+    pub fn none_with_capacity(states_capacity: usize) -> Self {
+        Self(Inner::NeverRan {
+            empty: VecDeque::with_capacity(states_capacity),
+        })
+    }
+    pub fn some_with_capacity(present: T, states_capacity: usize) -> Self {
+        Self(Inner::Ran {
+            log: StateLog::with_capacity(present, states_capacity),
+            undone_first_run: None,
+        })
+    }
+    pub fn into_inner(self) -> Option<T> {
+        match self.0 {
+            Inner::NeverRan { .. } => None,
+            Inner::Ran { log, .. } => Some(log.into_inner()),
+        }
     }
     pub fn get(&self) -> Option<&T> {
-        if let Inner::Ran {
-            log,
-            undone_first_run: false,
-        } = &self.0
-        {
-            Some(&*log)
-        } else {
-            None
-        }
-    }
-    pub fn get_log(&self) -> Option<&StateLog<T>> {
-        if let Inner::Ran {
-            log,
-            undone_first_run: false,
-        } = &self.0
-        {
-            Some(log)
-        } else {
-            None
-        }
-    }
-    pub fn get_log_unchecked(&self) -> Option<&StateLog<T>> {
-        if let Inner::Ran { log, .. } = &self.0 {
-            Some(log)
-        } else {
-            None
-        }
-    }
-    pub fn get_log_mut(&mut self) -> Option<&mut StateLog<T>> {
-        if let Inner::Ran {
-            log,
-            undone_first_run: false,
-        } = &mut self.0
-        {
-            Some(log)
-        } else {
-            None
-        }
-    }
-    pub fn get_log_mut_unchecked(&mut self) -> Option<&mut StateLog<T>> {
-        if let Inner::Ran { log, .. } = &mut self.0 {
-            Some(log)
-        } else {
-            None
-        }
-    }
-    pub fn push_present(&mut self, state: T) -> &mut StateLog<T> {
         match self.0 {
+            Inner::Ran { ref log, undone_first_run } if undone_first_run != Some(true) => {
+                Some(&*log)
+            },
+            _ => None
+        }
+    }
+    pub fn states_len(&self) -> usize {
+        match &self.0 {
+            Inner::NeverRan { .. } => 0,
+            Inner::Ran { log, .. } => log.states_len(),
+        }
+    }
+    pub fn states_capacity(&self) -> usize {
+        match &self.0 {
+            Inner::NeverRan { empty } => empty.capacity(),
+            Inner::Ran { log, .. } => log.states_capacity(),
+        }
+    }
+    pub fn states_is_empty(&self) -> bool {
+        match &self.0 {
+            Inner::NeverRan { .. } => true,
+            Inner::Ran { log, .. } => log.states_is_empty(),
+        }
+    }
+    pub fn states_reserve(&mut self, additional: usize) {
+        match &mut self.0 {
+            Inner::NeverRan { empty } => empty.reserve(additional),
+            Inner::Ran { log, .. } => log.states_reserve(additional),
+        }
+    }
+    pub fn states_reserve_exact(&mut self, additional: usize) {
+        match &mut self.0 {
+            Inner::NeverRan { empty } => empty.reserve_exact(additional),
+            Inner::Ran { log, .. } => log.states_reserve_exact(additional),
+        }
+    }
+    pub fn states_try_reserve(&mut self, additional: usize) -> Result<(), TryReserveError> {
+        match &mut self.0 {
+            Inner::NeverRan { empty } => empty.try_reserve(additional),
+            Inner::Ran { log, .. } => log.states_try_reserve(additional),
+        }
+    }
+    pub fn states_try_reserve_exact(&mut self, additional: usize) -> Result<(), TryReserveError> {
+        match &mut self.0 {
+            Inner::NeverRan { empty } => empty.try_reserve_exact(additional),
+            Inner::Ran { log, .. } => log.states_try_reserve_exact(additional),
+        }
+    }
+    pub fn states_shrink_to(&mut self, min_capacity: usize) {
+        match &mut self.0 {
+            Inner::NeverRan { empty } => empty.shrink_to(min_capacity),
+            Inner::Ran { log, .. } => log.states_shrink_to(min_capacity),
+        }
+    }
+    pub fn states_shrink_to_fit(&mut self) {
+        match &mut self.0 {
+            Inner::NeverRan { empty } => empty.shrink_to_fit(),
+            Inner::Ran { log, .. } => log.states_shrink_to_fit(),
+        }
+    }
+    pub fn push_present(&mut self, state: T) {
+        match &mut self.0 {
+            Inner::NeverRan { .. } => self.never_ran_push(state),
             Inner::Ran {
-                ref mut log,
-                ref mut undone_first_run,
+                log,
+                undone_first_run,
             } => {
-                if *undone_first_run {
+                if *undone_first_run == Some(true) {
                     log.clear_with(state);
-                    *undone_first_run = false;
+                    *undone_first_run = Some(false);
                 } else {
                     log.push_present(state)
                 }
-                log
-            }
-            Inner::NeverRan { capacity } => {
-                self.0 = Inner::Ran {
-                    log: StateLog::with_capacity(state, capacity),
-                    undone_first_run: false,
-                };
-                match &mut self.0 {
-                    Inner::Ran { log, .. } => log,
-                    Inner::NeverRan { .. } => unreachable!(),
-                }
             }
         }
     }
-    // todo: may incorrectly return `Ok` if `clear` was called before
-    pub fn forward_log(&mut self) -> Result<&mut StateLog<T>, OutOfLog> {
-        match &mut self.0 {
+    pub fn drain_future(&mut self) -> impl LogIter<T> {
+        if matches!(
+            self.0,
             Inner::Ran {
-                log,
-                undone_first_run,
-            } => {
-                if *undone_first_run {
-                    *undone_first_run = false;
-                    Ok(log)
-                } else {
-                    log.forward_log().map(|()| log)
-                }
+                undone_first_run: Some(true),
+                ..
             }
-            Inner::NeverRan { .. } => Err(OutOfLog),
+        ) {
+            let (oldest, mut log) = match take(self).0 {
+                Inner::Ran { log, .. } => log.into_inner_with_log(),
+                Inner::NeverRan { .. } => unreachable!(),
+            };
+            log.reserve_exact(1);
+            log.push_front(oldest);
+            self.0 = Inner::NeverRan { empty: log };
+            return match &mut self.0 {
+                Inner::NeverRan { empty } => empty.drain(..),
+                Inner::Ran { .. } => unreachable!(),
+            };
         }
-    }
-    // todo: may incorrectly return `Ok` if constructed from T
-    pub fn backward_log(&mut self) -> Result<&mut StateLog<T>, OutOfLog> {
         match &mut self.0 {
-            Inner::Ran {
-                log,
-                undone_first_run,
-            } => {
-                if *undone_first_run {
-                    Err(OutOfLog)
-                } else {
-                    *undone_first_run = log.backward_log() == Err(OutOfLog);
-                    Ok(log)
-                }
-            }
-            Inner::NeverRan { .. } => Err(OutOfLog),
+            Inner::NeverRan { empty } => empty.drain(..0),
+            Inner::Ran { log, .. } => log.drain_future_specific(),
         }
     }
-    pub fn clear(&mut self) {
+    pub fn clear_log(&mut self) {
         if let Inner::Ran {
             log,
-            undone_first_run,
+            ..
         } = &mut self.0
         {
             log.clear();
-            *undone_first_run = true;
         }
+    }
+    pub fn clear(&mut self) {
+        let empty = match take(self).0 {
+            Inner::NeverRan { empty } => empty,
+            Inner::Ran { log, .. } => log.into_inner_with_log().1
+        };
+        self.0 = Inner::NeverRan { empty };
     }
     pub fn clear_with(&mut self, present: T) {
         match &mut self.0 {
+            Inner::NeverRan { .. } => self.never_ran_push(present),
             Inner::Ran {
                 log,
                 undone_first_run,
             } => {
                 log.clear_with(present);
-                *undone_first_run = true;
+                *undone_first_run = None;
             }
-            Inner::NeverRan { capacity } => {
-                self.0 = Inner::Ran {
-                    log: StateLog::with_capacity(present, *capacity),
-                    undone_first_run: true,
+        }
+    }
+    pub fn backward_log(&mut self) -> Result<(), OutOfLog> {
+        match self.0 {
+            Inner::Ran { undone_first_run: Some(true), .. } => Err(OutOfLog),
+            Inner::Ran { ref mut log, undone_first_run: None } => {
+                log.backward_log()
+            },
+            Inner::Ran { ref mut log, ref mut undone_first_run} => {
+                *undone_first_run = Some(log.backward_log() == Err(OutOfLog));
+                Ok(())
+            },
+            Inner::NeverRan { .. } => Err(OutOfLog)
+        }
+    }
+    pub fn forward_log(&mut self) -> Result<(), OutOfLog> {
+        match self.0 {
+            Inner::Ran { undone_first_run: Some(ref mut undone), .. } if *undone => {
+                *undone = false;
+                Ok(())
+            },
+            Inner::Ran { ref mut log, .. } => log.forward_log(),
+            Inner::NeverRan { .. } => Err(OutOfLog)
+        }
+    }
+    pub fn pop_past_by_len(&mut self, max_past_len: usize) -> Option<T> {
+        match self.0 {
+            Inner::Ran { undone_first_run: Some(true), .. } | Inner::NeverRan { .. } => None,
+            Inner::Ran {
+                ref mut log,
+                ref mut undone_first_run
+            } => {
+                let popped = log.pop_past_by_len(max_past_len);
+                if popped.is_some() || log.past_len() == max_past_len {
+                    *undone_first_run = None;
                 }
+                popped
+            } 
+        }
+    }
+    pub fn drain_past_by_len(&mut self, max_past_len: usize) -> impl LogIter<T> {
+        match self.0 {
+            Inner::NeverRan { ref mut empty } => empty.drain(..0),
+            Inner::Ran {
+                ref mut log,
+                undone_first_run: Some(true),
+            } => log.drain_past_by_len_specific(usize::MAX),
+            Inner::Ran {
+                ref mut log, 
+                ref mut undone_first_run
+            } => {
+                let iter = log.drain_past_by_len_specific(max_past_len);
+                if iter.len() != 0 {
+                    *undone_first_run = None;
+                }
+                iter
+            }
+        }
+    }
+    fn never_ran_push(&mut self, present: T) {
+        let empty = match take(self).0 {
+            Inner::NeverRan { empty } => empty,
+            Inner::Ran { .. } => unreachable!(),
+        };
+        self.0 = Inner::Ran {
+            log: StateLog::with_alloc(present, empty),
+            undone_first_run: Some(false),
+        };
+    }
+}
+
+impl<T: LoggedAt> InitNoneLog<T> {
+    pub fn pop_past_by_logged_at(&mut self, meta: &RevMeta) -> Option<T> {
+        match self.0 {
+            Inner::Ran { undone_first_run: Some(true), .. } | Inner::NeverRan { .. } => None,
+            Inner::Ran { 
+                ref mut log, 
+                ref mut undone_first_run 
+            } => {
+                let popped = log.pop_past_by_logged_at(meta);
+                if popped.is_some() {
+                    *undone_first_run = None;
+                }
+                popped
+            }
+        }
+    }
+    pub fn truncate_future_drain_past_by_logged_at(&mut self, meta: &RevMeta) -> impl LogIter<T> {
+        match self.0 {
+            Inner::NeverRan { ref mut empty } => empty.drain(..0),
+            Inner::Ran {
+                ref mut log,
+                undone_first_run: Some(true),
+            } => log.drain_past_by_len_specific(usize::MAX),
+            Inner::Ran {
+                ref mut log, 
+                ref mut undone_first_run
+            } => {
+                let iter = log.truncate_future_drain_past_by_logged_at_specific(meta);
+                if iter.len() != 0 {
+                    *undone_first_run = None;
+                }
+                iter
             }
         }
     }
