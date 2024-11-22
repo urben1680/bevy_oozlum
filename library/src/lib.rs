82254e77e8c66753ee0d1509e738b271c2f52c4c
urben1680 <55257931+urben1680@users.noreply.github.com>
2024-11-22 17:19:37 +0100

    Update lib.rs

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -14,10 +14,15 @@ TODO:
 Enhancements:
 - reduce todo!() and //todo
 - RareInitNoneLog tests
-- pipe config tests
--- dummy systems in a_then_b einfügen und dummy system auch als ordnungssystem nutzen
--- dummy.pipe(sys) und sys.pipe(dummy) variationen
-- impl Error for relevant structs
+- impl Error for relevant structs/enums
+- #[inline]s
+- examples
+-- folder next to src
+--- how to have cargo check look at it?
+--- add CI
+-- hooks https://github.com/bevyengine/bevy/blob/main/examples/ecs/component_hooks.rs
+-- observers https://github.com/bevyengine/bevy/blob/main/examples/ecs/observers.rs
+-- ecs, commands https://github.com/bevyengine/bevy/blob/main/examples/ecs/ecs_guide.rs
 
 Docs
 - documentations
@@ -34,15 +39,11 @@ ISSUES/DISCUSSIONS:
 - manual sync point configuration
 -- apply_deferred
 -- ScheduleBuildSettings::auto_insert_apply_deferred
-- examples
 */
 
 use std::{fmt::Debug, hash::Hash};
 
-use bevy::{
-    ecs::{component::Tick, schedule::ScheduleLabel},
-    reflect::Reflect,
-};
+use bevy::{ecs::schedule::ScheduleLabel, reflect::Reflect};
 
 #[cfg(feature = "serde")]
 use bevy::reflect::{ReflectDeserialize, ReflectSerialize};
@@ -71,53 +72,6 @@ pub mod prelude {
     pub use crate::{PackedRevFrame, RevFrame, RevUpdate};
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Reflect, Hash)]
-#[reflect(Debug)]
-#[cfg_attr(feature = "serde", reflect(Serialize, Deserialize))]
-pub struct RevFrame(u32);
-
-impl From<RevFrame> for u32 {
-    fn from(value: RevFrame) -> Self {
-        value.0
-    }
-}
-
-impl RevFrame {
-    const fn new(value: u32) -> Self {
-        debug_assert!(value <= PackedRevFrame::MAX_AS_U32);
-        Self(value)
-    }
-    const fn wrapping_add(self, value: u32) -> Self {
-        Self(self.0.wrapping_add(value) & PackedRevFrame::MAX_AS_U32)
-    }
-    const fn wrapping_sub(self, value: u32) -> Self {
-        Self(self.0.wrapping_sub(value) & PackedRevFrame::MAX_AS_U32)
-    }
-    const fn first_half(self) -> bool {
-        self.0 <= PackedRevFrame::MAX_AS_U32 / 2
-    }
-}
-
-impl Debug for RevFrame {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
-    }
-}
-
-#[cfg(feature = "serde")]
-impl serde::Serialize for RevFrame {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        PackedRevFrame::from(*self).serialize(serializer)
-    }
-}
-
-#[cfg(feature = "serde")]
-impl<'de> serde::Deserialize<'de> for RevFrame {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        PackedRevFrame::deserialize(deserializer).map(Into::into)
-    }
-}
-
 #[cfg(feature = "packed_rev_frame_size_1")]
 const PACKED_REV_FRAME_SIZE: usize = 1;
 
@@ -134,72 +88,68 @@ const PACKED_REV_FRAME_SIZE: usize = 3;
 )))]
 const PACKED_REV_FRAME_SIZE: usize = 4;
 
+const REV_FRAME_AS_U32_MAX: u32 = {
+    let bits = PACKED_REV_FRAME_SIZE * 8;
+    let shift = 32 - bits;
+    u32::MAX >> shift
+};
+
+#[derive(Clone, Copy, PartialEq, Eq, Reflect, Hash)]
+#[reflect(Debug)]
+#[cfg_attr(feature = "serde", reflect(Serialize, Deserialize))]
+pub struct RevFrame(u32);
+
 #[derive(Clone, Copy, Reflect, PartialEq, Eq, Hash)]
 #[reflect(Debug)]
 #[cfg_attr(feature = "serde", reflect(Serialize, Deserialize))]
 pub struct PackedRevFrame([u8; PACKED_REV_FRAME_SIZE]);
 
-impl PackedRevFrame {
-    const MAX_AS_U32: u32 = {
-        let bits = PACKED_REV_FRAME_SIZE * 8;
-        let shift = 32 - bits;
-        u32::MAX >> shift
-    };
+impl RevFrame {
+    #[cfg(test)]
+    const fn checked_new(value: u32) -> Self {
+        assert!(value <= REV_FRAME_AS_U32_MAX);
+        Self(value)
+    }
+    const fn wrapping_add(self, value: u32) -> Self {
+        Self(self.0.wrapping_add(value) & REV_FRAME_AS_U32_MAX)
+    }
+    const fn wrapping_sub(self, value: u32) -> Self {
+        Self(self.0.wrapping_sub(value) & REV_FRAME_AS_U32_MAX)
+    }
+    const fn first_half(self) -> bool {
+        self.0 <= REV_FRAME_AS_U32_MAX / 2
+    }
+}
+
+impl From<RevFrame> for u32 {
+    fn from(value: RevFrame) -> Self {
+        value.0
+    }
 }
 
 impl From<PackedRevFrame> for u32 {
     fn from(value: PackedRevFrame) -> Self {
-        let mut i = value.0.into_iter();
-        u32::from_le_bytes(std::array::from_fn(|_| i.next().unwrap_or(0)))
-    }
-}
-
-impl Debug for PackedRevFrame {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        u32::from(*self).fmt(f)
-    }
-}
-
-#[cfg(feature = "serde")]
-impl serde::Serialize for PackedRevFrame {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        u32::from(*self).serialize(serializer)
-    }
-}
-
-#[cfg(feature = "serde")]
-impl<'de> serde::Deserialize<'de> for PackedRevFrame {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let value: u32 = u32::deserialize(deserializer)?;
-        if value <= Self::MAX_AS_U32 {
-            let mut i = value.to_le_bytes().into_iter();
-            Ok(Self(std::array::from_fn(|_| i.next().unwrap_or(0))))
-        } else {
-            Err(serde::de::Error::custom(format!(
-                // todo update after changing to u32
-                "{value} does not fit into {} bytes, cannot map this value to `PackedRevFrame` \
-                on this machine, increase the `time_bytes_*` feature of the reversible_systems \
-                crate to the value of the source where this value was serialized or, if the \
-                source does not use that feature, change that to a value low enough to be \
-                supported by all machines",
-                PACKED_REV_FRAME_SIZE,
-            )))
-        }
+        RevFrame::from(value).0
     }
 }
 
 impl From<PackedRevFrame> for RevFrame {
     fn from(value: PackedRevFrame) -> Self {
-        RevFrame(u32::from_le_bytes(value.0))
+        Self(u32::from_le_bytes(resize_le_bytes(value.0)))
     }
 }
 
 impl From<RevFrame> for PackedRevFrame {
     fn from(value: RevFrame) -> Self {
-        // RevFrame is only constructed from u32 <= PackedRevFrame::MAX_AS_U32
-        let mut i = value.0.to_le_bytes().into_iter();
-        Self(std::array::from_fn(|_| i.next().unwrap_or(0)))
+        Self(resize_le_bytes(value.0.to_le_bytes()))
     }
+}
+
+/// Assumes cut-off bytes, if any, are `0`.
+#[inline(always)]
+fn resize_le_bytes<const N: usize, const M: usize>(arr: [u8; N]) -> [u8; M] {
+    let mut i = arr.into_iter();
+    std::array::from_fn(|_| i.next().unwrap_or(0))
 }
 
 impl PartialEq<RevFrame> for PackedRevFrame {
@@ -216,17 +166,62 @@ impl PartialEq<PackedRevFrame> for RevFrame {
     }
 }
 
-#[derive(ScheduleLabel, Copy, Clone, Debug, Hash, PartialEq, Eq)]
-pub struct RevUpdate;
-
-/// reference: Tick::check_tick
-// todo: move to module this is using if there ends up being only one
-fn check_tick(this: &mut Tick, change_tick: Tick) {
-    let age = change_tick.get().wrapping_sub(this.get());
-    if age > Tick::MAX.get() {
-        *this = Tick::new(change_tick.get().wrapping_sub(Tick::MAX.get()));
+impl Debug for RevFrame {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
     }
 }
+
+impl Debug for PackedRevFrame {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        u32::from(*self).fmt(f)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for RevFrame {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        PackedRevFrame::from(*self).serialize(serializer)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for PackedRevFrame {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        u32::from(*self).serialize(serializer)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for RevFrame {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        PackedRevFrame::deserialize(deserializer).map(Into::into)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for PackedRevFrame {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let value: u32 = u32::deserialize(deserializer)?;
+        if value <= REV_FRAME_AS_U32_MAX {
+            let mut i = value.to_le_bytes().into_iter();
+            Ok(Self(std::array::from_fn(|_| i.next().unwrap_or(0))))
+        } else {
+            Err(serde::de::Error::custom(format!(
+                // todo update after changing to u32
+                "{value} does not fit into {} bytes, cannot map this value to `PackedRevFrame` \
+                on this machine, increase the `time_bytes_*` feature of the reversible_systems \
+                crate to the value of the source where this value was serialized or, if the \
+                source does not use that feature, change that to a value low enough to be \
+                supported by all machines",
+                PACKED_REV_FRAME_SIZE,
+            )))
+        }
+    }
+}
+
+#[derive(ScheduleLabel, Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub struct RevUpdate;
 
 macro_rules! error_per_flag {
     ($flag:expr, $($arg:tt)+) => ({
