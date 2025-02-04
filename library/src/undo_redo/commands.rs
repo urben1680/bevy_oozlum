2093ca478b7f6266505f5e1de305cb2dc7e5e002
urben1680 <55257931+urben1680@users.noreply.github.com>
2025-02-04 16:56:30 +0100

    Update commands.rs

--- library/src/undo_redo/commands.rs
+++ library/src/undo_redo/commands.rs
@@ -1,6 +1,6 @@
 use bevy::ecs::{
-    system::{Commands, Resource},
-    world::{FromWorld, World},
+    result::Error,
+    bundle::Bundle, component::ComponentId, entity::{Entity, EntityCloneBuilder}, resource::Resource, system::{entity_command::CommandWithEntity, Commands, EntityCommand, EntityCommands}, world::{FromWorld, World}
 };
 
 use super::{BuffersUndoRedo, UndoRedo};
@@ -53,5 +53,233 @@ impl<R: Resource> UndoRedo for ResourceSwap<R> {
     }
     fn redo(&mut self, world: &mut World) {
         self.undo(world)
+    }
+}
+
+pub trait RevEntityCommands {
+    /// Reversible version of [`EntityCommands::insert`].
+    fn rev_insert(&mut self, bundle: impl Bundle) -> &mut Self;
+    /// Reversible version of [`EntityCommands::insert_if`].
+    fn rev_insert_if<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self 
+    where
+        F: FnOnce() -> bool;
+    /// Reversible version of [`EntityCommands::insert_if_new`].
+    fn rev_insert_if_new(&mut self, bundle: impl Bundle) -> &mut Self;
+    /// Reversible version of [`EntityCommands::insert_if_new_and`].
+    fn rev_insert_if_new_and<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self
+    where
+        F: FnOnce() -> bool;
+    /// Reversible version of [`EntityCommands::insert_by_id`].
+    unsafe fn rev_insert_by_id<T: Send + 'static>(
+        &mut self,
+        component_id: ComponentId,
+        value: T,
+    ) -> &mut Self;
+    /// Reversible version of [`EntityCommands::try_insert_by_id`].
+    unsafe fn rev_try_insert_by_id<T: Send + 'static>(
+        &mut self,
+        component_id: ComponentId,
+        value: T,
+    ) -> &mut Self;
+    /// Reversible version of [`EntityCommands::try_insert`].
+    fn rev_try_insert(&mut self, bundle: impl Bundle) -> &mut Self;
+    /// Reversible version of [`EntityCommands::try_insert_if`].
+    fn rev_try_insert_if<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self
+    where
+        F: FnOnce() -> bool;
+    /// Reversible version of [`EntityCommands::try_insert_if_new_and`].
+    fn rev_try_insert_if_new_and<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self
+    where
+        F: FnOnce() -> bool;
+    /// Reversible version of [`EntityCommands::try_insert_if_new`].
+    fn rev_try_insert_if_new(&mut self, bundle: impl Bundle) -> &mut Self;
+    /// Reversible version of [`EntityCommands::remove`].
+    fn rev_remove<T>(&mut self) -> &mut Self
+    where
+        T: Bundle;
+    /// Reversible version of [`EntityCommands::try_remove`].
+    fn rev_try_remove<T>(&mut self) -> &mut Self
+    where
+        T: Bundle;
+    /// Reversible version of [`EntityCommands::remove_with_requires`].
+    fn rev_remove_with_requires<T: Bundle>(&mut self) -> &mut Self;
+    /// Reversible version of [`EntityCommands::remove_by_id`].
+    fn rev_remove_by_id(&mut self, component_id: ComponentId) -> &mut Self;
+    /// Reversible version of [`EntityCommands::clear`].
+    fn rev_clear(&mut self) -> &mut Self;
+    /// Reversible version of [`EntityCommands::despawn`].
+    fn rev_despawn(&mut self);
+    /// Reversible version of [`EntityCommands::despawn_recursive`].
+    fn rev_despawn_recursive(&mut self);
+    /// Reversible version of [`EntityCommands::try_despawn`].
+    fn rev_try_despawn(&mut self);
+    /// Reversible version of [`EntityCommands::queue`].
+    fn rev_queue<C: EntityCommand<T> + CommandWithEntity<M>, T, M>(
+        &mut self,
+        command: C,
+    ) -> &mut Self;
+    /// Reversible version of [`EntityCommands::queue_handled`].
+    fn rev_queue_handled<C: EntityCommand<T> + CommandWithEntity<M>, T, M>(
+        &mut self,
+        command: C,
+        error_handler: fn(&mut World, Error),
+    ) -> &mut Self;
+    /// Reversible version of [`EntityCommands::retain`].
+    fn rev_retain<T>(&mut self) -> &mut Self
+    where
+        T: Bundle;
+    /// Reversible version of [`EntityCommands::clone_with`].
+    fn rev_clone_with(
+        &mut self,
+        target: Entity,
+        config: impl FnOnce(&mut EntityCloneBuilder) + Send + Sync + 'static,
+    ) -> &mut Self;
+    /// Reversible version of [`EntityCommands::clone_and_spawn`].
+    fn rev_clone_and_spawn(&mut self) -> EntityCommands<'_>;
+    /// Reversible version of [`EntityCommands::clone_and_spawn_with`].
+    fn rev_clone_and_spawn_with(
+        &mut self,
+        config: impl FnOnce(&mut EntityCloneBuilder) + Send + Sync + 'static,
+    ) -> EntityCommands<'_>;
+    /// Reversible version of [`EntityCommands::clone_components`].
+    fn rev_clone_components<B: Bundle>(&mut self, target: Entity) -> &mut Self;
+    /// Reversible version of [`EntityCommands::move_components`].
+    fn rev_move_components<B: Bundle>(&mut self, target: Entity) -> &mut Self;
+}
+
+impl RevEntityCommands for EntityCommands<'_> {
+    fn rev_insert(&mut self, bundle: impl Bundle) -> &mut Self {
+        todo!()
+    }
+    fn rev_insert_if<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self 
+    where
+        F: FnOnce() -> bool
+    {
+        if condition() {
+            self.rev_insert(bundle)
+        } else {
+            self
+        }
+    }
+    fn rev_insert_if_new(&mut self, bundle: impl Bundle) -> &mut Self {
+        todo!()
+    }
+    fn rev_insert_if_new_and<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self
+    where
+        F: FnOnce() -> bool,
+    {
+        if condition() {
+            self.rev_insert_if_new(bundle)
+        } else {
+            self
+        }
+    }
+    unsafe fn rev_insert_by_id<T: Send + 'static>(
+        &mut self,
+        component_id: ComponentId,
+        value: T,
+    ) -> &mut Self {
+        todo!()
+    }
+    unsafe fn rev_try_insert_by_id<T: Send + 'static>(
+        &mut self,
+        component_id: ComponentId,
+        value: T,
+    ) -> &mut Self {
+        todo!()
+    }
+    fn rev_try_insert(&mut self, bundle: impl Bundle) -> &mut Self {
+        todo!()
+    }
+    fn rev_try_insert_if<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self
+    where
+        F: FnOnce() -> bool,
+    {
+        if condition() {
+            self.rev_try_insert(bundle)
+        } else {
+            self
+        }
+    }
+    fn rev_try_insert_if_new_and<F>(&mut self, bundle: impl Bundle, condition: F) -> &mut Self
+    where
+        F: FnOnce() -> bool,
+    {
+        if condition() {
+            self.rev_try_insert_if_new(bundle)
+        } else {
+            self
+        }
+    }
+    fn rev_try_insert_if_new(&mut self, bundle: impl Bundle) -> &mut Self {
+        todo!()
+    }
+    fn rev_remove<T>(&mut self) -> &mut Self
+    where
+        T: Bundle {
+        todo!()
+    }
+    fn rev_try_remove<T>(&mut self) -> &mut Self
+    where
+        T: Bundle {
+        todo!()
+    }
+    fn rev_remove_with_requires<T: Bundle>(&mut self) -> &mut Self {
+        todo!()
+    }
+    fn rev_remove_by_id(&mut self, component_id: ComponentId) -> &mut Self {
+        todo!()
+    }
+    fn rev_clear(&mut self) -> &mut Self {
+        todo!()
+    }
+    fn rev_despawn(&mut self) {
+        todo!()
+    }
+    fn rev_despawn_recursive(&mut self) {
+        todo!()
+    }
+    fn rev_try_despawn(&mut self) {
+        todo!()
+    }
+    fn rev_queue<C: EntityCommand<T> + CommandWithEntity<M>, T, M>(
+        &mut self,
+        command: C,
+    ) -> &mut Self {
+        todo!()
+    }
+    fn rev_queue_handled<C: EntityCommand<T> + CommandWithEntity<M>, T, M>(
+        &mut self,
+        command: C,
+        error_handler: fn(&mut World, Error),
+    ) -> &mut Self {
+        todo!()
+    }
+    fn rev_retain<T>(&mut self) -> &mut Self
+    where
+        T: Bundle {
+        todo!()
+    }
+    fn rev_clone_with(
+        &mut self,
+        target: Entity,
+        config: impl FnOnce(&mut EntityCloneBuilder) + Send + Sync + 'static,
+    ) -> &mut Self {
+        todo!()
+    }
+    fn rev_clone_and_spawn(&mut self) -> EntityCommands<'_> {
+        todo!()
+    }
+    fn rev_clone_and_spawn_with(
+        &mut self,
+        config: impl FnOnce(&mut EntityCloneBuilder) + Send + Sync + 'static,
+    ) -> EntityCommands<'_> {
+        todo!()
+    }
+    fn rev_clone_components<B: Bundle>(&mut self, target: Entity) -> &mut Self {
+        todo!()
+    }
+    fn rev_move_components<B: Bundle>(&mut self, target: Entity) -> &mut Self {
+        todo!()
     }
 }
