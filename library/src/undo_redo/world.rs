c6ae415411471bc12f5de3f44dfe59d726504639
urben1680 <55257931+urben1680@users.noreply.github.com>
2025-04-17 16:56:17 +0200

    Update world.rs

--- library/src/undo_redo/world.rs
+++ library/src/undo_redo/world.rs
@@ -1,25 +1,24 @@
 use std::{
     any::TypeId,
     hash::{BuildHasher, Hash, Hasher},
-    marker::PhantomData,
     sync::Arc,
 };
 
 use bevy::{
     ecs::{
         bundle::{Bundle, BundleId, NoBundleEffect},
-        component::{ComponentCloneBehavior, ComponentId, ComponentMutability},
-        entity::{Entity, EntityCloner},
+        component::ComponentId,
+        entity::Entity,
         resource::Resource,
-        world::{EntityRef, EntityWorldMut, FromWorld, Mut, World},
+        world::{EntityRef, EntityWorldMut, FromWorld, World},
     },
-    platform_support::{
-        collections::{HashMap, HashSet},
+    platform::{
+        collections::HashMap,
         hash::{FixedHasher, PassHash},
     },
 };
 
-use crate::{meta::RevMeta, unique_for_location};
+use crate::meta::RevMeta;
 
 use super::*;
 
@@ -129,14 +128,7 @@ impl RevWorld for World {
         let batch: Vec<_> = batch.into_iter().collect();
         for &(entity, _) in batch.iter() {
             let archetype_id = self.entities().get(entity).unwrap().archetype_id;
-            self.buffer_components_cached(
-                entity,
-                unique_for_location!(archetype_id, PhantomData::<B>),
-                |world| {
-                    let bundle_id = world.register_bundle::<B>().id();
-                    insert_maybe_overwrite(world, bundle_id, archetype_id)
-                },
-            );
+            pre_insert::<B>(self, entity, archetype_id, InsertMode::Replace);
         }
         self.insert_batch(batch);
     }
@@ -153,28 +145,15 @@ impl RevWorld for World {
         self.insert_batch_if_new(batch.inspect(|(entity, _)| entities.push(*entity)));
         for entity in entities {
             let archetype_id = self.entities().get(entity).unwrap().archetype_id;
-            self.buffer_components_cached(
-                entity,
-                unique_for_location!(archetype_id, PhantomData::<B>),
-                |world| {
-                    let bundle_id = world.register_bundle::<B>().id();
-                    insert_no_overwrite(world, bundle_id, archetype_id)
-                },
-            );
+            pre_insert::<B>(self, entity, archetype_id, InsertMode::Keep);
         }
     }
 
     fn rev_spawn<B: Bundle>(&mut self, bundle: B) -> EntityWorldMut {
         let meta = self.get_resource::<RevMeta>().expect("todo");
         let marker = DespawnAtOutOfLog::new(meta);
-        let bundle_id = self.register_bundle::<B>().id();
         let mut entity_mut = self.spawn(bundle);
         let entity = entity_mut.id();
-        let world = unsafe {
-            // SAFETY: only resources are mutated
-            entity_mut.world_mut()
-        };
-        buffer_bundle(world, entity, BufferAt::Undo, bundle_id);
         entity_mut.buffer_undo_redo(Spawn { entity, marker });
         entity_mut
     }
@@ -287,262 +266,6 @@ impl RevWorld for World {
 
     fn buffer_components_in_progress(&self) -> bool {
         self.contains_resource::<BufferComponentsInProgress>()
-    }
-}
-
-#[derive(Resource, Default)]
-struct NonEntityBufferRes(HashMap<ComponentId, fn(&mut World, Entity, BufferAt)>);
-
-fn non_entity_buffer(world: &mut World, entity: Entity, at: BufferAt, components: &[ComponentId]) {
-    if !world.contains_resource::<NonEntityBufferRes>() {
-        return;
-    }
-    world.resource_scope(|world, non_entity_buffers: Mut<NonEntityBufferRes>| {
-        for component in components.iter() {
-            if let Some(c) = non_entity_buffers.0.get(component) {
-                c(world, entity, at);
-            }
-        }
-    })
-}
-
-pub(crate) fn register_non_entity_buffer<T: Component>(world: &mut World) {
-    struct NonEntityBuffer<T: Component> {
-        entity: Entity,
-        component: Option<T>,
-    }
-
-    impl<T: Component> UndoRedo for NonEntityBuffer<T> {
-        fn undo(&mut self, world: &mut World) {
-            let mut entity = world.entity_mut(self.entity);
-            if T::Mutability::MUTABLE {
-                let component = unsafe {
-                    // SAFETY: this if branch asserts the component is mutable
-                    entity.get_mut_assume_mutable::<T>()
-                };
-                match component {
-                    Some(mut c1) => match self.component.as_mut() {
-                        Some(c2) => core::mem::swap(&mut *c1, c2),
-                        None => self.component = entity.take::<T>(),
-                    },
-                    None => {
-                        if let Some(c2) = self.component.take() {
-                            entity.insert(c2);
-                        }
-                    }
-                }
-            } else {
-                match entity.take::<T>() {
-                    Some(mut c1) => match self.component.as_mut() {
-                        Some(c2) => {
-                            core::mem::swap(&mut c1, c2);
-                            entity.insert(c1);
-                        }
-                        None => self.component = Some(c1),
-                    },
-                    None => {
-                        if let Some(c2) = self.component.take() {
-                            entity.insert(c2);
-                        }
-                    }
-                }
-            }
-        }
-        fn redo(&mut self, world: &mut World) {
-            self.undo(world);
-        }
-    }
-
-    let component_id = world.register_component::<T>();
-    world
-        .get_resource_or_init::<NonEntityBufferRes>()
-        .0
-        .entry(component_id)
-        .or_insert_with(|| {
-            |world, entity, at| {
-                let mut component = None;
-                if at != BufferAt::Undo {
-                    component = world.entity_mut(entity).take::<T>();
-                }
-                let undo_redo = NonEntityBuffer { entity, component };
-                world.buffer_undo_redo(undo_redo);
-            }
-        });
-}
-
-struct Spawn {
-    entity: Entity,
-    marker: DespawnAtOutOfLog,
-}
-
-impl UndoRedo for Spawn {
-    fn undo(&mut self, world: &mut World) {
-        world.entity_mut(self.entity).insert(self.marker);
-    }
-    fn redo(&mut self, world: &mut World) {
-        world.entity_mut(self.entity).remove::<DespawnAtOutOfLog>();
-    }
-}
-
-// todo: replace this with register_dynamic_bundle when linked issue is fixed
-fn components_to_bundle(world: &mut World, components: Vec<ComponentId>) -> BundleId {
-    #[derive(Resource, Default)]
-    struct CheckedClonable(HashSet<ComponentId>);
-
-    let mut checked = world
-        .remove_resource::<CheckedClonable>()
-        .unwrap_or_default();
-    for &component_id in &components {
-        if checked.0.insert(component_id) {
-            if let Some(component_info) = world.components().get_info(component_id) {
-                if component_info.clone_behavior() == &ComponentCloneBehavior::Ignore {
-                    bevy::log::error!(
-                        "Component {} is unclonable, it's insert, remove or overwrite will \
-                        not be reversible, see https://github.com/bevyengine/bevy/issues/18079",
-                        component_info.name()
-                    );
-                }
-            }
-        }
-    }
-    world.insert_resource(checked);
-
-    world.register_dynamic_bundle(&components).id()
-}
-
-fn buffer_bundle(
-    world: &mut World,
-    entity: Entity,
-    at: BufferAt,
-    bundle: BundleId,
-) -> Option<EntityRef> {
-    let mut buffer = BundleBuffer::new(world, entity, bundle);
-    match at {
-        BufferAt::Now => {
-            let entities = buffer.toggle_state(world);
-            let components = buffer.get_component_ids(world);
-            non_entity_buffer(world, entity, at, &components);
-            let out = buffer.move_bundle(world, entities, &components);
-            world.buffer_undo_redo(buffer);
-            Some(world.entity(out))
-        }
-        BufferAt::Undo => {
-            let components = buffer.get_component_ids(world);
-            non_entity_buffer(world, entity, at, &components);
-            world.buffer_undo_redo(buffer);
-            None
-        }
-        BufferAt::NowAndUndo => {
-            let at_undo = buffer.clone(); // no buffer entity set yet so each spawns their own
-            let entities = buffer.toggle_state(world);
-            let components = buffer.get_component_ids(world);
-            non_entity_buffer(world, entity, at, &components);
-            let out = buffer.move_bundle(world, entities, &components);
-            world.buffer_undo_redo(buffer).buffer_undo_redo(at_undo);
-            Some(world.entity(out))
-        }
-    }
-}
-
-#[derive(Clone)]
-struct BundleBuffer {
-    bundle: BundleId,
-    entity: Entity,
-    state: BufferState,
-}
-
-#[derive(Clone)]
-enum BufferState {
-    Unspawned(DespawnAtOutOfLog),
-    Empty(Entity),
-    Filled(Entity),
-}
-
-struct BundleEntities {
-    target: Entity,
-    source: Entity,
-    buffer: Entity,
-}
-
-impl BundleBuffer {
-    fn new(world: &World, entity: Entity, bundle: BundleId) -> Self {
-        let meta = world.get_resource::<RevMeta>().expect("todo");
-        let marker = DespawnAtOutOfLog::new(meta);
-        Self {
-            bundle,
-            entity,
-            state: BufferState::Unspawned(marker),
-        }
-    }
-    fn toggle_state(&mut self, world: &mut World) -> BundleEntities {
-        match self.state {
-            BufferState::Unspawned(marker) => {
-                let buffer = world.spawn(marker).id();
-                self.state = BufferState::Filled(buffer);
-                BundleEntities {
-                    target: buffer,
-                    source: self.entity,
-                    buffer,
-                }
-            }
-            BufferState::Filled(buffer) => {
-                self.state = BufferState::Empty(buffer);
-                BundleEntities {
-                    target: self.entity,
-                    source: buffer,
-                    buffer,
-                }
-            }
-            BufferState::Empty(buffer) => {
-                self.state = BufferState::Filled(buffer);
-                BundleEntities {
-                    target: buffer,
-                    source: self.entity,
-                    buffer,
-                }
-            }
-        }
-    }
-    fn get_component_ids(&self, world: &World) -> Box<[ComponentId]> {
-        world
-            .bundles()
-            .get(self.bundle)
-            .expect("todo")
-            .explicit_components()
-            .into()
-    }
-    fn move_bundle(
-        &mut self,
-        world: &mut World,
-        entities: BundleEntities,
-        components: &[ComponentId],
-    ) -> Entity {
-        let progress_res = world.buffer_components_in_progress();
-        if !progress_res {
-            world.insert_resource(BufferComponentsInProgress);
-        }
-        EntityCloner::build(world)
-            .deny_all()
-            .move_components(true)
-            .without_required_components(|builder| {
-                builder.allow_by_ids(components.iter().copied());
-            })
-            .clone_entity(entities.source, entities.target);
-        if !progress_res {
-            world.remove_resource::<BufferComponentsInProgress>();
-        }
-        entities.buffer
-    }
-}
-
-impl UndoRedo for BundleBuffer {
-    fn undo(&mut self, world: &mut World) {
-        let entities = self.toggle_state(world);
-        let components = self.get_component_ids(world);
-        self.move_bundle(world, entities, &components);
-    }
-    fn redo(&mut self, world: &mut World) {
-        self.undo(world);
     }
 }
 
