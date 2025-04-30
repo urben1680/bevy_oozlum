15b92fb1ff1091ce2f764e17fb8b8877ae47e248
urben1680 <55257931+urben1680@users.noreply.github.com>
2025-04-30 15:52:15 +0200

    Update mod.rs

--- library/src/undo_redo/world/mod.rs
+++ library/src/undo_redo/world/mod.rs
@@ -1,23 +1,14 @@
-use std::{
-    any::TypeId,
-    hash::{BuildHasher, Hash, Hasher},
-    sync::Arc,
-};
+use std::{hash::Hash, sync::Arc};
 
 use bevy::{
     ecs::{
         bundle::{Bundle, BundleId, NoBundleEffect},
-        change_detection::MaybeLocation,
         component::ComponentId,
-        entity::{Entity, EntityLocation},
+        entity::Entity,
         resource::Resource,
-        world::{EntityWorldMut, FromWorld, World, error::TryInsertBatchError},
+        world::{EntityWorldMut, FromWorld, World},
     },
     log::warn,
-    platform::{
-        collections::HashMap,
-        hash::{FixedHasher, PassHash},
-    },
 };
 
 use crate::meta::RevMeta;
@@ -28,6 +19,8 @@ use super::*;
 mod test;
 
 pub trait RevWorld {
+    // buffer methods
+
     fn buffer_components_in_progress(&self) -> Option<BufferInProgress>;
 
     fn buffer_components(
@@ -51,6 +44,18 @@ pub trait RevWorld {
         bundle: BundleId,
     ) -> Result<Option<Entity>, RevEntityError>;
 
+    // additional fallible methods
+
+    fn rev_try_spawn<B: Bundle>(&mut self, bundle: B)
+    -> Result<EntityWorldMut, RevMetaNotLogError>;
+
+    fn rev_try_spawn_empty(&mut self) -> Result<EntityWorldMut, RevMetaNotLogError>;
+
+    fn rev_try_spawn_batch<I>(&mut self, iter: I) -> Result<Arc<[Entity]>, RevMetaNotLogError>
+    where
+        I: IntoIterator,
+        I::Item: Bundle<Effect: NoBundleEffect>;
+
     // the methods here are purposely sorted alphabetically to make it easily comparable to bevy's docs
     // unmentioned methods are either
     // a) unrelated to reversible structural changes OR
@@ -58,7 +63,12 @@ pub trait RevWorld {
     // c) missed by accident!
 
     /// Reversible version of [`World::despawn`].
-    fn rev_despawn(&mut self, entity: Entity) -> bool;
+    #[track_caller]
+    fn rev_despawn(&mut self, entity: Entity) -> bool {
+        self.rev_try_despawn(entity)
+            .inspect_err(|err| warn!("entity {entity} could not be reversibly despawned: {err}"))
+            .is_ok()
+    }
 
     /// Reversible version of [`World::get_resource_or_init`].
     fn rev_get_resource_or_init<R: Resource + FromWorld>(&mut self) -> Mut<'_, R>;
@@ -76,18 +86,28 @@ pub trait RevWorld {
     fn rev_init_resource<R: Resource + FromWorld>(&mut self);
 
     /// Reversible version of [`World::insert_batch`].
+    #[track_caller]
     fn rev_insert_batch<I, B>(&mut self, batch: I)
     where
         I: IntoIterator,
         I::IntoIter: Iterator<Item = (Entity, B)>,
-        B: Bundle<Effect: NoBundleEffect>;
+        B: Bundle<Effect: NoBundleEffect>,
+    {
+        self.rev_try_insert_batch(batch)
+            .unwrap_or_else(|err| panic!("{err}"))
+    }
 
     /// Reversible version of [`World::insert_batch_if_new`].
+    #[track_caller]
     fn rev_insert_batch_if_new<I, B>(&mut self, batch: I)
     where
         I: IntoIterator,
         I::IntoIter: Iterator<Item = (Entity, B)>,
-        B: Bundle<Effect: NoBundleEffect>;
+        B: Bundle<Effect: NoBundleEffect>,
+    {
+        self.rev_try_insert_batch_if_new(batch)
+            .unwrap_or_else(|err| panic!("{err}"))
+    }
 
     // rev_insert_non_send_by_id
     // out of scope due Send bound on UndoRedo
@@ -111,16 +131,29 @@ pub trait RevWorld {
     // blocked on https://github.com/bevyengine/bevy/pull/17485
 
     /// Reversible version of [`World::spawn`].
-    fn rev_spawn<B: Bundle>(&mut self, bundle: B) -> EntityWorldMut;
+    #[track_caller]
+    fn rev_spawn<B: Bundle>(&mut self, bundle: B) -> EntityWorldMut {
+        self.rev_try_spawn(bundle)
+            .unwrap_or_else(|err| panic!("{err}"))
+    }
 
     /// Reversible version of [`World::spawn_batch`].
+    #[track_caller]
     fn rev_spawn_batch<I>(&mut self, iter: I) -> Arc<[Entity]>
     where
         I: IntoIterator,
-        I::Item: Bundle<Effect: NoBundleEffect>;
+        I::Item: Bundle<Effect: NoBundleEffect>,
+    {
+        self.rev_try_spawn_batch(iter)
+            .unwrap_or_else(|err| panic!("{err}"))
+    }
 
     /// Reversible version of [`World::spawn_empty`].
-    fn rev_spawn_empty(&mut self) -> EntityWorldMut<'_>;
+    #[track_caller]
+    fn rev_spawn_empty(&mut self) -> EntityWorldMut<'_> {
+        self.rev_try_spawn_empty()
+            .unwrap_or_else(|err| panic!("{err}"))
+    }
 
     /// Reversible version of [`World::try_despawn`].
     fn rev_try_despawn(&mut self, entity: Entity) -> Result<(), RevEntityError>;
@@ -138,23 +171,6 @@ pub trait RevWorld {
         I: IntoIterator,
         I::IntoIter: Iterator<Item = (Entity, B)>,
         B: Bundle<Effect: NoBundleEffect>;
-
-    // additional fallible methods
-
-    fn rev_try_spawn<B: Bundle>(
-        &mut self,
-        bundle: B,
-    ) -> Result<EntityWorldMut, RevSpawnError>;
-
-    fn rev_try_spawn_empty(&mut self) -> Result<EntityWorldMut, RevSpawnError>;
-
-    fn rev_try_spawn_batch<I>(
-        &mut self,
-        iter: I,
-    ) -> Result<Arc<[Entity]>, RevSpawnError>
-    where
-        I: IntoIterator,
-        I::Item: Bundle<Effect: NoBundleEffect>;
 }
 
 impl RevWorld for World {
@@ -162,64 +178,50 @@ impl RevWorld for World {
         buffer_components_in_progress(self)
     }
 
-    fn rev_despawn(&mut self, entity: Entity) -> bool {
-        self.rev_try_despawn(entity)
-            .inspect_err(|error| warn!("{}", BufferBundleError {
-                entity,
-
-            }))
-            .is_ok()
+    fn buffer_components(
+        &mut self,
+        entity: Entity,
+        at: BufferAt,
+        components: &[ComponentId],
+    ) -> Result<Option<Entity>, RevEntityError> {
+        let bundle = components_to_bundle(self, components);
+        self.buffer_bundle(entity, at, bundle)
     }
 
-    fn rev_insert_batch<I, B>(&mut self, batch: I)
-    where
-        I: IntoIterator,
-        I::IntoIter: Iterator<Item = (Entity, B)>,
-        B: Bundle<Effect: NoBundleEffect>,
-    {
-        let batch: Vec<_> = batch.into_iter().collect();
-        for &(entity, _) in batch.iter() {
-            let archetype_id = self.entities().get(entity).unwrap().archetype_id;
-            pre_insert::<B>(self, entity, archetype_id, InsertMode::Replace).expect("todo");
-        }
-        self.insert_batch(batch);
+    fn buffer_components_cached<T: AsRef<[ComponentId]>>(
+        &mut self,
+        entity: Entity,
+        key: impl Hash + 'static,
+        components: impl FnOnce(&mut World) -> (BufferAt, T),
+    ) -> Result<Option<Entity>, RevEntityError> {
+        let marker = DespawnAtOutOfLog::for_buffer(self.get_resource::<RevMeta>())?;
+        buffer_components_cached(self, entity, key, components, marker)
     }
 
-    fn rev_insert_batch_if_new<I, B>(&mut self, batch: I)
-    where
-        I: IntoIterator,
-        I::IntoIter: Iterator<Item = (Entity, B)>,
-        B: Bundle<Effect: NoBundleEffect>,
-    {
-        let batch = batch.into_iter();
-        let (min, max) = batch.size_hint();
-        let mut entities = Vec::with_capacity(max.unwrap_or(min));
-        self.insert_batch_if_new(batch.inspect(|(entity, _)| entities.push(*entity)));
-        for entity in entities {
-            let archetype_id = self.entities().get(entity).unwrap().archetype_id;
-            pre_insert::<B>(self, entity, archetype_id, InsertMode::Keep).expect("todo");
-        }
+    fn buffer_bundle(
+        &mut self,
+        entity: Entity,
+        at: BufferAt,
+        bundle: BundleId,
+    ) -> Result<Option<Entity>, RevEntityError> {
+        let marker = DespawnAtOutOfLog::for_buffer(self.get_resource::<RevMeta>())?;
+        buffer_bundle(self, entity, at, bundle, marker)
     }
 
+    #[track_caller]
     fn rev_try_spawn<B: Bundle>(
         &mut self,
         bundle: B,
-    ) -> Result<EntityWorldMut, RevSpawnError> {
-        let marker = DespawnAtOutOfLog::new(self.get_resource::<RevMeta>())?;
+    ) -> Result<EntityWorldMut, RevMetaNotLogError> {
+        let marker = DespawnAtOutOfLog::for_spawn_despawn(self.get_resource::<RevMeta>())?;
         let mut entity_mut = self.spawn(bundle);
         let entity = entity_mut.id();
         entity_mut.buffer_undo_redo(Spawn { entity, marker });
         Ok(entity_mut)
     }
 
-    fn rev_spawn<B: Bundle>(&mut self, bundle: B) -> EntityWorldMut {
-        self.rev_try_spawn(bundle).expect("todo")
-    }
-
-    fn rev_try_spawn_batch<I>(
-        &mut self,
-        iter: I,
-    ) -> Result<Arc<[Entity]>, RevSpawnError>
+    #[track_caller]
+    fn rev_try_spawn_batch<I>(&mut self, iter: I) -> Result<Arc<[Entity]>, RevMetaNotLogError>
     where
         I: IntoIterator,
         I::Item: Bundle<Effect: NoBundleEffect>,
@@ -241,39 +243,22 @@ impl RevWorld for World {
             }
         }
 
-        match DespawnAtOutOfLog::new(self.get_resource::<RevMeta>()) {
-            Ok(marker) => {
-                let entities: Arc<[Entity]> = self.spawn_batch(iter).collect();
-                self.buffer_undo_redo(SpawnBatch {
-                    entities: entities.clone(),
-                    marker,
-                });
-                Ok(entities)
-            }
-            Err(err) => Err((iter, err)),
-        }
+        let marker = DespawnAtOutOfLog::for_spawn_despawn(self.get_resource::<RevMeta>())?;
+        let entities: Arc<[Entity]> = self.spawn_batch(iter).collect();
+        self.buffer_undo_redo(SpawnBatch {
+            entities: entities.clone(),
+            marker,
+        });
+        Ok(entities)
     }
 
-    fn rev_spawn_batch<I>(&mut self, iter: I) -> Arc<[Entity]>
-    where
-        I: IntoIterator,
-        I::Item: Bundle<Effect: NoBundleEffect>,
-    {
-        self.rev_try_spawn_batch(iter)
-            .map_err(|(_, err)| err)
-            .expect("todo")
-    }
-
-    fn rev_try_spawn_empty(&mut self) -> Result<EntityWorldMut, RevSpawnError> {
-        let marker = DespawnAtOutOfLog::new(self.get_resource::<RevMeta>())?;
+    #[track_caller]
+    fn rev_try_spawn_empty(&mut self) -> Result<EntityWorldMut, RevMetaNotLogError> {
+        let marker = DespawnAtOutOfLog::for_spawn_despawn(self.get_resource::<RevMeta>())?;
         let mut entity_mut = self.spawn_empty();
         let entity = entity_mut.id();
         entity_mut.buffer_undo_redo(Spawn { entity, marker });
         Ok(entity_mut)
-    }
-
-    fn rev_spawn_empty(&mut self) -> EntityWorldMut<'_> {
-        self.rev_try_spawn_empty().expect("todo")
     }
 
     fn rev_init_resource<R: Resource + FromWorld>(&mut self) {
@@ -297,60 +282,32 @@ impl RevWorld for World {
         })
     }
 
-    fn buffer_components(
-        &mut self,
-        entity: Entity,
-        at: BufferAt,
-        components: &[ComponentId],
-    ) -> Result<Option<Entity>, RevEntityError> {
-        let bundle = components_to_bundle(self, components);
-        self.buffer_bundle(entity, at, bundle)
+    #[track_caller]
+    fn rev_try_despawn(&mut self, entity: Entity) -> Result<(), RevEntityError> {
+        rev_despawn_inner(self.get_entity_mut(entity).map_err(|err| match err {
+            EntityMutableFetchError::EntityDoesNotExist(err) => err,
+            EntityMutableFetchError::AliasedMutability(_) => unreachable!(),
+        })?)
     }
 
-    fn buffer_components_cached<T: AsRef<[ComponentId]>>(
-        &mut self,
-        entity: Entity,
-        key: impl Hash + 'static,
-        components: impl FnOnce(&mut World) -> (BufferAt, T),
-    ) -> Result<Option<Entity>, RevEntityError> {
-        #[derive(Resource, Default)]
-        pub(crate) struct CachedBundles(HashMap<u64, (BufferAt, BundleId), PassHash>);
-        fn type_id_of_var<T: 'static>(_: &T) -> TypeId {
-            TypeId::of::<T>()
-        }
-
-        let mut hasher = FixedHasher::default().build_hasher();
-        type_id_of_var(&key).hash(&mut hasher);
-        key.hash(&mut hasher);
-        let key = hasher.finish();
-
-        let mut cache = self.remove_resource::<CachedBundles>().unwrap_or_default();
-        let (at, bundle) = *cache.0.entry(key).or_insert_with(|| {
-            let (at, components) = components(self);
-            let components = components.as_ref();
-            (at, components_to_bundle(self, &components))
-        });
-        self.insert_resource(cache);
-        self.buffer_bundle(entity, at, bundle)
+    #[track_caller]
+    fn rev_try_insert_batch<I, B>(&mut self, batch: I) -> Result<(), RevEntitiesError>
+    where
+        I: IntoIterator,
+        I::IntoIter: Iterator<Item = (Entity, B)>,
+        B: Bundle<Effect: NoBundleEffect>,
+    {
+        try_insert_batch_inner(self, batch, InsertMode::Replace)
     }
 
-    fn buffer_bundle(
-        &mut self,
-        entity: Entity,
-        at: BufferAt,
-        bundle: BundleId,
-    ) -> Result<Option<Entity>, RevEntityError> {
-        let location = MaybeLocation::caller();
-        match DespawnAtOutOfLog::new(self.get_resource::<RevMeta>()) {
-            Ok(marker) => buffer_bundle(self, entity, at, bundle, marker, location),
-            Err(err) => Err(BufferBundleError {
-                entity,
-                at,
-                bundle,
-                variant: err.into(),
-                location,
-            }),
-        }
+    #[track_caller]
+    fn rev_try_insert_batch_if_new<I, B>(&mut self, batch: I) -> Result<(), RevEntitiesError>
+    where
+        I: IntoIterator,
+        I::IntoIter: Iterator<Item = (Entity, B)>,
+        B: Bundle<Effect: NoBundleEffect>,
+    {
+        try_insert_batch_inner(self, batch, InsertMode::Keep)
     }
 
     fn rev_get_resource_or_init<R: Resource + FromWorld>(&mut self) -> Mut<'_, R> {
@@ -367,57 +324,35 @@ impl RevWorld for World {
         }
         self.get_resource_or_insert_with(func)
     }
-
-    #[track_caller]
-    fn rev_try_despawn(&mut self, entity: Entity) -> Result<(), RevEntityError> {
-        let entity_mut = self.get_entity_mut(entity)?;
-        rev_despawn_inner(entity_mut)
-    }
-
-    fn rev_try_insert_batch<I, B>(&mut self, batch: I) -> Result<(), RevEntitiesError>
-    where
-        I: IntoIterator,
-        I::IntoIter: Iterator<Item = (Entity, B)>,
-        B: Bundle<Effect: NoBundleEffect>,
-    {
-        try_insert_batch_inner(self, batch, InsertMode::Replace)
-    }
-
-    fn rev_try_insert_batch_if_new<I, B>(&mut self, batch: I) -> Result<(), RevEntitiesError>
-    where
-        I: IntoIterator,
-        I::IntoIter: Iterator<Item = (Entity, B)>,
-        B: Bundle<Effect: NoBundleEffect>,
-    {
-        try_insert_batch_inner(self, batch, InsertMode::Keep)
-    }
 }
 
+#[track_caller]
 fn try_insert_batch_inner<I, B>(
     world: &mut World,
     batch: I,
     insert_mode: InsertMode,
-) -> Result<(), TryInsertBatchError>
+) -> Result<(), RevEntitiesError>
 where
     I: IntoIterator,
     I::IntoIter: Iterator<Item = (Entity, B)>,
     B: Bundle<Effect: NoBundleEffect>,
 {
-    let mut invalid_entities = Vec::new();
+    let marker = DespawnAtOutOfLog::for_buffer(world.get_resource::<RevMeta>())?;
+    let mut invalid = Vec::new();
+    let mut rev_despawned = Vec::new();
     let batch: Vec<_> = batch
         .into_iter()
         .filter(|&(entity, _)| {
             world
-                .entities()
-                .get(entity)
-                .filter(|&EntityLocation { archetype_id, .. }| {
-                    pre_insert::<B>(world, entity, archetype_id, insert_mode).is_ok()
-                })
-                .or_else(|| {
-                    invalid_entities.push(entity);
-                    None
-                })
-                .is_some()
+                .get_entity(entity)
+                .map_err(|err| invalid.push(err))
+                .map(|entity| entity.location().archetype_id)
+                .is_ok_and(|archetype_id| pre_insert::<B>(world, entity, archetype_id, insert_mode, marker)
+                    .map_err(|err| match err {
+                        RevEntityError::EntityRevDespawnedError(err) => rev_despawned.push(err),
+                        _ => unreachable!("EntityDoesNotExistError collected earlier, no other errors returned by pre_insert"),
+                    })
+                    .is_ok())
         })
         .collect();
     match insert_mode {
@@ -425,12 +360,12 @@ where
         InsertMode::Keep => world.insert_batch_if_new(batch),
     }
 
-    if invalid_entities.is_empty() {
+    if invalid.is_empty() && rev_despawned.is_empty() {
         Ok(())
     } else {
-        Err(TryInsertBatchError {
-            bundle_type: core::any::type_name::<B>(),
-            entities: invalid_entities,
+        Err(RevEntitiesError::BadEntities {
+            invalid,
+            rev_despawned,
         })
     }
 }
