4b6cc9fe8fd57b98d30634a0e7dd32876d33d19a
urben1680 <55257931+urben1680@users.noreply.github.com>
2026-05-18 22:31:20 +0200

    update

--- examples/jump_to_frame/main.rs
+++ examples/jump_to_frame/main.rs
@@ -1,6 +1,6 @@
-use bevy::{prelude::*, window::WindowResolution};
-use bevy_oozlum::{prelude::*, schedule::run_rev_update};
-use std::{fmt::Write, mem::take};
+use bevy::prelude::*;
+use bevy_oozlum::prelude::*;
+use std::mem::take;
 
 // This example shows jumping to a specific frame in the log instead of always the log end
 
@@ -8,12 +8,11 @@ fn main() {
     App::new()
         .add_plugins((
             RevPlugin.set_max_past_len(9),
-            DefaultPlugins.set(window_plugin()),
+            DefaultPlugins.set(render::window_plugin()),
+            render::plugin,
         ))
-        .add_systems(Startup, setup)
         .add_systems(PreUpdate, control)
         .add_systems(RevUpdate, update_frame_log.after(RevSystems))
-        .add_systems(FixedUpdate, render.after(run_rev_update))
         .init_resource::<FrameLog>()
         .insert_resource(Time::<Fixed>::from_seconds(0.33))
         .run();
@@ -85,73 +84,4 @@ fn control(
     }
 }
 
-fn window_plugin() -> WindowPlugin {
-    WindowPlugin {
-        primary_window: Some(Window {
-            resizable: false,
-            resolution: WindowResolution::new(430, 550),
-            ..Default::default()
-        }),
-        ..Default::default()
-    }
-}
-
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2d);
-    commands.spawn((
-        Text::default(),
-        TextLayout::no_wrap(),
-        Node {
-            flex_direction: FlexDirection::Column,
-            align_self: AlignSelf::Center,
-            justify_self: JustifySelf::Center,
-            align_items: AlignItems::Center,
-            ..Default::default()
-        },
-    ));
-}
-
-fn render(meta: Res<RevMeta>, frame_log: Res<FrameLog>, mut text: Single<&mut Text>) -> Result {
-    text.clear();
-
-    writeln!(
-        &mut text.0,
-        "UP: backward log, pause at end
-DOWN: forward log, pause at end
-RIGHT: exit log and resume
-LEFT: pause
-ESCAPE: close"
-    )?;
-
-    if meta
-        .get_ran_direction()
-        .is_some_and(RevDirection::is_not_log)
-    {
-        write!(&mut text.0, "SPACE: mark frame to pause at")?;
-    }
-
-    match meta.get_ran_direction() {
-        None => writeln!(&mut text.0, "\n\nPause\n\n-- log start --"),
-        Some(direction) => writeln!(&mut text.0, "\n\n{direction}\n\n-- log start --"),
-    }?;
-
-    for frame in meta.past_end()..=meta.future_end() {
-        let marked = match frame_log.marked_frames.binary_search(&frame).is_ok() {
-            false => ' ',
-            true => 'X',
-        };
-        let now = match frame == meta.now() {
-            false => ' ',
-            true => '<',
-        };
-        writeln!(&mut text.0, "{frame:05} {marked} {now}")?;
-    }
-
-    for _ in meta.len()..10 {
-        writeln!(&mut text.0)?;
-    }
-
-    write!(&mut text.0, "-- log end --")?;
-
-    Ok(())
-}
+mod render;
---
+++ examples/jump_to_frame/render.rs
@@ -0,0 +1,83 @@
+use bevy::{prelude::*, window::WindowResolution};
+use bevy_oozlum::{prelude::*, schedule::run_rev_update};
+use std::fmt::Write;
+
+use super::FrameLog;
+
+// Not many comments here as this is not really a showcase of the crate's functionalities
+
+pub fn plugin(app: &mut App) {
+    app.add_systems(Startup, setup)
+        .add_systems(FixedUpdate, render.after(run_rev_update));
+}
+
+pub fn window_plugin() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(Window {
+            resizable: false,
+            resolution: WindowResolution::new(430, 550),
+            ..Default::default()
+        }),
+        ..Default::default()
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Text::default(),
+        TextLayout::no_wrap(),
+        Node {
+            flex_direction: FlexDirection::Column,
+            align_self: AlignSelf::Center,
+            justify_self: JustifySelf::Center,
+            align_items: AlignItems::Center,
+            ..Default::default()
+        },
+    ));
+}
+
+fn render(meta: Res<RevMeta>, frame_log: Res<FrameLog>, mut text: Single<&mut Text>) -> Result {
+    text.clear();
+
+    writeln!(
+        &mut text.0,
+        "UP: backward log, pause at end
+DOWN: forward log, pause at end
+RIGHT: exit log and resume
+LEFT: pause
+ESCAPE: close"
+    )?;
+
+    if meta
+        .get_ran_direction()
+        .is_some_and(RevDirection::is_not_log)
+    {
+        write!(&mut text.0, "SPACE: mark frame to pause at")?;
+    }
+
+    match meta.get_ran_direction() {
+        None => writeln!(&mut text.0, "\n\nPause\n\n-- log start --"),
+        Some(direction) => writeln!(&mut text.0, "\n\n{direction}\n\n-- log start --"),
+    }?;
+
+    for frame in meta.past_end()..=meta.future_end() {
+        let marked = match frame_log.marked_frames.binary_search(&frame).is_ok() {
+            false => ' ',
+            true => 'X',
+        };
+        let now = match frame == meta.now() {
+            false => ' ',
+            true => '<',
+        };
+        writeln!(&mut text.0, "{frame:05} {marked} {now}")?;
+    }
+
+    for _ in meta.len()..10 {
+        writeln!(&mut text.0)?;
+    }
+
+    write!(&mut text.0, "-- log end --")?;
+
+    Ok(())
+}
